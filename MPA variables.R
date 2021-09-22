8cddd8bbe904c9b90e87af16a95d05efac0e15ea
jacobeurich <jacobeurich@ucsb.edu>
2021-09-22 16:59:17 -0700

    Question

--- MPA variables.R
+++ MPA variables.R
@@ -1,12 +1,10 @@
 # MPA variables
 # ---
-# code by Jacob Eurich, Sept 22 2021
-
+# code by Jacob Eurich, Sept. 22 2021
 
 # Initialization ----------------------------------------------------------
 data_path <- "/home/shares/ca-mpa/MPA variable data/"
-
 mpavar <- read.csv(file="California MPA variables for evaluation analyses.csv", header=TRUE)
 
-
+## JB, can you show me how to pull the csv file from cyberduck?
 
