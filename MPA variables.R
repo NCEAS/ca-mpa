c84e6561ae7fa226aed253ba8ee9c1800dfb1070
jacobeurich <jacobeurich@ucsb.edu>
2021-09-22 16:33:18 -0700

    New R script for MPA vairblaes

---
+++ MPA variables.R
@@ -0,0 +1,8 @@
+# MPA variables
+# ---
+# code by Jacob Eurich, Sept 22 2021
+
+
+# Initialization ----------------------------------------------------------
+data_path <- "/home/shares/ca-mpa/raw data (test)/"
+
