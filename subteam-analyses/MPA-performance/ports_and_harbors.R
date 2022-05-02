7bc1e6e4375737e7874d99b75aca1d81a9151e09
Joshua Smith <joshsmith@nceas.ucsb.edu>
2022-05-02 16:08:20 -0700

    script that calculates linear distance to nearest port or harbor

---
+++ subteam-analyses/MPA-performance/ports_and_harbors.R
@@ -0,0 +1,131 @@
+rm(list = ls())
+
+
+
+require(sf)
+require(tidyverse)
+
+
+#load CDFW mpa polygons
+data_path <- "/home/shares/ca-mpa/data/sync-data/CA_MPA_boundaries/ds582"
+input_file <- "ds582.shp" 
+
+location.data <- st_read(input_file, stringsAsFactors = F)
+
+data.sf <- st_transform(x = location.data, crs=4326) #transform to WGS84
+
+#calcualte centroids
+data.sf$centroids <- data.sf %>%
+                            st_centroid() %>%
+                            st_geometry() 
+
+
+plot(st_geometry(data.sf))
+plot(st_set_geometry(data.sf, 'centroids')[,0], add = T, col = 'red', pch = 3)
+
+#st_coordinates(data.sf$centroids)
+
+#add coordinates to data frame
+
+
+separated_coord <- data.sf %>%
+    mutate(Name = NAME,
+           long = unlist(map(data.sf$centroids,1)),
+           lat = unlist(map(data.sf$centroids,2))
+           ) %>%
+    select(Name, lat, long)
+
+
+
+#read in mpa-attribute table and port locations
+
+data_path <- "/home/shares/ca-mpa/data/sync-data"
+input_file <- "mpa-attributes.xlsx" 
+
+mpa.attributes <- readxl::read_excel(file.path(data_path, input_file), sheet=1, skip = 0, na="NA")
+
+input_file <- "ports_and_harbors.xlsx" 
+port.locations <- readxl::read_excel(file.path(data_path, input_file), sheet=1, skip = 0, na="NA")
+
+mpa.coords <- left_join(mpa.attributes,separated_coord,
+                        by = 'Name')  %>%
+              drop_na("lat")
+
+#clean up new mpa-attribute table
+
+mpa.attrib <- mpa.coords %>%
+              select(-c("Latitude","Annual_citations","Enforcement_metric","Compliance_estimate","Scientific_permits","Distance_to_port"))
+              
+
+
+
+
+
+#================calculate closest port for each MPA
+
+mpa.attrib.sp <- st_as_sf(mpa.attrib, coords = c("long","lat"), crs = 4326) #load into WGS84
+
+#transform to TA
+
+mpa.attrib.albers <- st_transform(mpa.attrib.sp, crs=3310)
+st_crs(mpa.attrib.albers)
+
+
+ports <- st_as_sf(port.locations, coords = c("long","lat"), crs = 4326) #load into WGS84
+ports.albers <- st_transform(ports, crs=3310) #transform to albers
+
+  
+
+#add coordinates for points in dataframes --------------------------------------
+a_coord <- st_coordinates(mpa.attrib.albers)
+a <- cbind(mpa.attrib.sp, a_coord)
+
+b_coord <- st_coordinates(ports.albers)
+b <- cbind(ports, b_coord)
+
+#get closes feature in B to A
+
+A_B <- a %>%
+  st_join(b %>%
+            select(port, size, X, Y) %>%
+            rename(B_X = X, B_Y = Y), join = st_nearest_feature)
+
+
+#create a WKT from the coords of A and closes feature in B ---------------------
+
+A_B$line_wkt <- paste('linestring(',A_B$X,A_B$Y,',',A_B$B_X, A_B$B_Y,')')
+
+
+#Converty WKT into Geom---------------------------------------------------------
+
+A_B <- A_B %>%
+  st_drop_geometry() %>%
+  st_as_sf(wkt = 'line_wkt', crs = 3310)
+
+
+#Get the length (distance of each line in meters)-------------------------------
+
+A_B$length <- as.numeric(st_length(A_B))
+
+#Join results with original A---------------------------------------------------
+mpa_attributes <- a %>%
+  left_join(A_B %>%
+              st_drop_geometry() %>%
+              select(Name, port, size, length), by = 'Name') 
+
+mpa_attributes <-  mpa_attributes %>%
+                    mutate(
+                      distance_to_port = length,
+                      long = unlist(map(mpa_attributes$geometry,1)),
+                      lat = unlist(map(mpa_attributes$geometry,2))) %>%
+                      select(-c("length", "geometry")) 
+
+
+View(mpa_attributes)
+
+
+
+
+
+plot(st_geometry(a), col = 'red', pch = 3) #mpa locations
+plot(st_geometry(b), add = T, col = 'black', pch = 17) #ports and harbors
