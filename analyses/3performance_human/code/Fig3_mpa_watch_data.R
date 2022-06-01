22e0312d139f1cbbf736447112c38380dbffb478
Chris Free <cfree14@gmail.com>
2022-06-01 11:27:20 -0700

    MPA watch work

--- analyses/3performance_human/code/Fig3_mpa_watch_data.R
+++ analyses/3performance_human/code/Fig3_mpa_watch_data.R
@@ -15,22 +15,18 @@ gisdir <- file.path(basedir, "gis_data/processed")
 datadir <- file.path(basedir, "mpa_watch/processed")
 plotdir <- "analyses/3performance_human/figures"
 
-# Read data
-state_waters_poly <- readRDS(file.path(gisdir, "CA_state_waters_polygons.Rds"))
-state_waters_line <- readRDS(file.path(gisdir, "CA_state_waters_polyline.Rds"))
-mpas_orig <- readRDS(file.path(gisdir, "CA_MPA_polygons.Rds"))
-
 # Get land
 usa <- rnaturalearth::ne_states(country="United States of America", returnclass = "sf")
 foreign <- rnaturalearth::ne_countries(country=c("Canada", "Mexico"), returnclass = "sf")
 
-# Reduce to MPAs of interest
-sort(unique(mpas_orig$type))
+# Read MPA data
+mpas_orig <- readRDS(file.path(basedir, "mpa_traits/processed", "CA_mpa_metadata.Rds"))
 types_use <- c("SMR", "SMRMA", "SMCA", "SMCA (No-Take)")
 mpas <- mpas_orig %>% 
   filter(type %in% types_use)
-mpas_df <- mpas %>% 
-  sf::st_drop_geometry()
+
+# To do list
+# Cull based on start time and end time
 
 
 # Build data
@@ -43,11 +39,11 @@ col_key <- readxl::read_excel(file.path(datadir, "column_key_ca_programs.xlsx"))
 # Build data
 data <- data_orig %>% 
   # Simplify
-  select(survey_id, mpa_name, mpa_id, date, duration_hr, total_activities:comments) %>% 
+  select(survey_id, mpa, mpa_id, survey_type, date, duration_hr, total_activities:comments) %>% 
   # Remove invalid surveys
   filter(duration_hr > 0) %>% 
   # Gather
-  gather(key="use", value="n_obs", 6:ncol(.)) %>% 
+  gather(key="use", value="n_obs", 7:ncol(.)) %>% 
   # Left join
   left_join(col_key) %>% 
   # Reduce to data of interest
@@ -57,10 +53,72 @@ data <- data_orig %>%
   # Reduce
   filter(!is.na(n_obs) & n_obs>0)
 
-# Summarize data
-surveys <- data %>% 
+# Examine survey coverage
+################################################################################
+
+# Examine survey coverage
+survey_coverage <- data %>% 
   # Number of activies on each survey by location/type
-  group_by(survey_id, mpa_name, mpa_id, date, duration_hr, location, consumptive) %>% 
+  select(survey_id, mpa, mpa_id, survey_type, date) %>% 
+  unique() %>% 
+  # Add year, month, dummy date
+  mutate(year=lubridate::year(date),
+         month=lubridate::month(date),
+         date_dummy=lubridate::ymd(paste(year, month, 1, sep="-"))) %>% 
+  # Number of surveys
+  group_by(year, month, date_dummy, mpa, mpa_id, survey_type) %>% 
+  summarize(nsurveys=n_distinct(survey_id)) %>% 
+  ungroup()
+
+# Number of MPAs and control sites
+n_distinct(data$survey_id)
+n_distinct(survey_coverage$mpa[survey_coverage$survey_type=="MPA"])
+n_distinct(survey_coverage$mpa[survey_coverage$survey_type=="Control"])
+
+# Theme
+theme1 <-  theme(axis.text=element_text(size=6),
+                 axis.text.y=element_text(size=5),
+                axis.title=element_text(size=8),
+                axis.title.y=element_blank(),
+                legend.text=element_text(size=6),
+                legend.title=element_text(size=7),
+                strip.text=element_text(size=7),
+                # Gridlines
+                panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(), 
+                axis.line = element_line(colour = "black"),
+                # Legend
+                legend.background = element_rect(fill=alpha('blue', 0)))
+
+# Plot survey coverage
+g <- ggplot(survey_coverage, aes(x=date_dummy, y=mpa, fill=nsurveys)) +
+  facet_grid(survey_type~., space="free_y", scales="free_y") +
+  geom_tile(color="grey30", lwd=0.05) +
+  # Add vertical lines
+  geom_vline(xintercept="")
+  # Labels
+  labs(x="Month", y="") +
+  # Legend
+  scale_fill_gradientn(name="# of surveys", colors=RColorBrewer::brewer.pal(9, "Blues"), trans="log2") +
+  guides(fill = guide_colorbar(ticks.colour = "black", frame.colour = "black")) +
+  # Theme
+  theme_bw() + theme1
+g
+
+# Export plot
+ggsave(g, filename=file.path(plotdir, "FigSX_mpa_watch_survey_coverage.png"), 
+       width=6.5, height=7, units="in", dpi=600)
+
+
+
+# Examine survey coverage
+################################################################################
+
+# Summarize data
+data1 <- data %>% 
+  # Number of activies on each survey by location/type
+  group_by(survey_id, mpa, mpa_id, survey_type, date, duration_hr, location, consumptive) %>% 
   summarize(n_activities=sum(n_obs)) %>% 
   ungroup() %>% 
   # Number per hour 
--- data/mpa_watch/Step3_format_mpa_watch_data_ca_programs.R
+++ data/mpa_watch/Step3_format_mpa_watch_data_ca_programs.R
@@ -21,22 +21,39 @@ data_orig <- read.csv(file.path(indir, "Surveys_2022-05-09_CaliforniaPrograms.cs
 # Read column key
 col_key <- readxl::read_excel(file.path(outdir, "column_key_ca_programs.xlsx"))
 
+# Read MPA data
+mpas <- readRDS(file.path(basedir, "mpa_traits/processed", "CA_mpa_metadata.Rds"))
+
 # To-do list
-# 1) Clean uses and add use categories
-# 2) Determine wind speed units
-# 3) The temperature column is messed up - handle if you care?
+# 1) Determine wind speed units
+# 2) The temperature column is messed up - handle if you care?
 # 3) Clean weather/tide station names/coordinates -- probably optional
-
-
 
 # Site key
 ################################################################################
 
 # Site key
 site_key <- data_orig %>% 
+  # Unique sites
   janitor::clean_names("snake") %>% 
   select(mpa, mpa_id) %>% 
-  unique()
+  unique() %>% 
+  # Recode a few MPA names
+  mutate(mpa=recode(mpa,
+                    "Blue Cavern (Catalina Island) SMCA"="Blue Cavern Offshore SMCA",
+                    "Cat Harbor (Catalina Island) SMCA"="Cat Harbor SMCA",
+                    "Laguna Beach SMCA"="Laguna Beach SMCA (No-Take)",
+                    "Año Nuevo SMCA"="Año Nuevo SMR",
+                    "Point Vicente SMCA"="Point Vicente SMCA (No-Take)",
+                    "Campus Point SMCA"="Campus Point SMCA (No-Take)",
+                    "Lovers Point SMR"="Lovers Point - Julia Platt SMR")) %>%
+  # Mark MPA or control
+  mutate(survey_type=ifelse(!grepl("control", tolower(mpa)), "MPA", "Control")) %>% 
+  # Format control names
+  mutate(mpa=gsub("CONTROL |control |Control |", "", mpa))
+
+# Confirm that all MPAs are in MPA key
+site_key$mpa[!site_key$mpa %in% mpas$mpa & site_key$survey_type=="MPA"]
   
 
 
@@ -48,9 +65,11 @@ data <- data_orig %>%
   # Rename
   janitor::clean_names("snake") %>% 
   # Rename columns
-  rename(mpa_name=mpa,
-         tide_ft=tide_height,
+  rename(tide_ft=tide_height,
          air_temp_f=air_temperature) %>%
+  # Add corrected MPA name and site type
+  select(-mpa) %>% 
+  left_join(site_key, by="mpa_id") %>% 
   # Convert date/time
   mutate(date=lubridate::ymd(date),
          time_start1=lubridate::hms(time_start),
@@ -87,14 +106,16 @@ data <- data_orig %>%
   # Compute survey duration
   mutate(duration_hr=lubridate::time_length(time_end1-time_start1, unit="hours")) %>% 
   # Arrange
-  select(survey_id:time_end, time_start1, time_end1, duration_hr, everything())
+  select(survey_id, program, mpa_id, mpa, survey_type, 
+         survey_site:time_end, time_start1, time_end1, duration_hr, everything())
 
 # Inspect data
 str(data)
-# freeR::complete(data)
+freeR::complete(data)
 
 # Inspect data
 table(data$program)
+table(data$survey_type)
 table(data$survey_site_type)
 table(data$beach_status)
 table(data$clouds)
@@ -130,7 +151,7 @@ tide_key <- data %>%
 # Gather
 data_long <- data %>% 
   # Gather
-  gather(key="use", value="n_obs", 30:ncol(.))
+  gather(key="use", value="n_obs", 31:ncol(.))
 
 # Export data
 ################################################################################
