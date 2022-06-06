8c09e182c0b6c10269fecd089964704c31eb69aa
lopazanski <cori.lopazanski@gmail.com>
2022-06-06 14:06:33 -0700

    add cdfw mpa attributes processing

---
+++ data/mpa_traits/process_cdfw_mpa_attributes.R
@@ -0,0 +1,54 @@
+###############################################################################
+# CDFW MPA Attributes Data 
+#      1. Process Raw Data
+#           Input:  mpa_attributes.csv
+#           Output: mpa_attributes_clean.csv
+################################################################################
+
+# Packages 
+library(tidyverse)
+library(janitor)
+library(here)
+
+# Directories
+# Cori Local Base:
+base.dir <- "/Volumes/GoogleDrive/Shared drives/NCEAS MPA network assessment/MPA Network Assessment: Working Group Shared Folder/data/sync-data"
+
+# Aurora Base:
+#base.dir <- "/home/shares/ca-mpa/data/sync-data/"
+
+in.dir <- here::here(base.dir, "mpa_traits", "raw")
+out.dir <- here::here(base.dir, "mpa_traits", "processed")
+plot.dir <- here::here(base.dir, "figures")
+
+# Read Raw Data
+raw <- read_csv(file.path(in.dir, "mpa_attributes.csv"), na = c("", "na"))
+
+################################################################################
+# Processing
+################################################################################
+data <- raw %>% 
+  # Clean variable names (mainly dashes in depth ranges)
+  clean_names() %>% 
+  
+  # Calculate total amount of hard substrate
+  mutate(total_hard_substrate = 
+           rowSums(select(., c(hard_substrate_predicted_0_30m_km2, 
+                               hard_substrate_30_100m_km2, 
+                               hard_substrate_100_200m_km2,
+                               hard_substrate_200_3000m_km2)))) %>% 
+  
+  # Calculate total amount of soft substrate
+  mutate(total_soft_substrate = 
+           rowSums(select(., c(soft_substrate_predicted_0_30m_km2, 
+                               soft_substrate_30_100m_km2, 
+                               soft_substrate_100_200m_km2, 
+                               soft_substratecal_200_3000m_km2)))) %>% 
+  
+  # Calculate depth range
+  mutate(depth_range = max_depth_m - min_depth_m)
+
+################################################################################
+# Export
+################################################################################
+write_csv(data, file.path(out.dir, "mpa_attributes_clean.csv"))
