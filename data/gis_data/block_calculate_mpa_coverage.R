b6ca4ec6a86f497a76565aa4b3c2cfa8d5a9292d
lopazanski <cori.lopazanski@gmail.com>
2022-07-04 09:08:33 -0700

    compile block covariates

--- data/gis_data/block_calculate_mpa_coverage.R
+++ data/gis_data/block_calculate_mpa_coverage.R
@@ -10,18 +10,70 @@ rm(list = ls())
 library(tidyverse)
 
 ## Directories
-basedir <- "/Volumes/GoogleDrive-105151121202188525604/Shared drives/NCEAS MPA network assessment/MPA Network Assessment: Working Group Shared Folder/data/sync-data" # Cori Local
-gisdir <- file.path(basedir, "gis_data/processed")
-plotdir <- "data/gis_data/figures"
+base.dir <- "/Volumes/GoogleDrive-105151121202188525604/Shared drives/NCEAS MPA network assessment/MPA Network Assessment: Working Group Shared Folder/data/sync-data" # Cori Local
+gis.dir <- file.path(base.dir, "gis_data/processed")
+out.dir <- file.path(gis.dir, "CA_block_polygons_buffers")
+plot.dir <- "data/gis_data/figures"
 
   
 ## Read Data ----
-block_mpa_key <- read.csv(file.path(gisdir, "CA_blocks_with_mpas.csv"), as.is=T)
 
-## Calculate MPA Proportion ----
-block_mpa_key <- block_mpa_key %>% 
+### Read Block Stats
+block_stats <- readRDS(file.path(gis.dir,"CA_blocks_stats.Rds"))
+
+### Read MPA Data
+mpas <- readRDS(file.path(gis.dir, "CA_MPA_polygons.Rds"))
+
+## Calculate block-level MPA proportion ----
+block_key <- block_stats %>% 
   mutate(percent_mpa = mpa_km2/block_area_km2*100)
 
+## Steps ----
+
+# Create buffered blocks - they will overlap with each other
+# Read the shapefile
+# Only look at SMRs/SMCAs and FMRs, FMCAs
+# Simplify MPAs/blocks
+# Calculate block area
+# Intersect MPAs/blocks
+# Calculate MPA area
+# 
+
+# Buffer block data -------------------------------------------------------------
+
+# Buffer distances
+buffers_km <- c(0.5, 1, 3, 5, 10, 20, 50)
+
+# Loop through buffer distances
+for(i in 1:length(buffers_km)){
+  
+  # Buffer
+  buffer_do <- buffers_km[i]
+  
+  # Buffer MPAs
+  block_buffer <- sf::st_buffer(block_stats, dist=buffer_do*1000)
+  
+  # Plot data
+  if(F){
+    g <- ggplot() +
+      geom_sf(data=block_stats, color="black", fill=NA) +
+      geom_sf(data=block_buffer, color="grey30", fill=NA)
+    g
+  }
+  
+  # Export data
+  filename <- paste0("CA_block_polygons_", buffer_do, "km_buffer.Rds")
+  saveRDS(block_buffer, file.path(out.dir,  filename))
+  
+}
+
+## Read Buffer
+buf_3k <- readRDS(file.path(out.dir, "CA_block_polygons_3km_buffer.Rds"))
+
+
+ggplot() +
+  geom_sf(data=block_stats, color="black") +
+  geom_sf(data=buf_3k, color="grey30")
 
 # Histograms with Raw Area -----------------------------------------------------
 
