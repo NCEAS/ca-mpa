82a5be2bdeeba37477417dbafea4826a7929f99d
Chris Free <cfree14@gmail.com>
2022-08-10 12:57:02 -0700

    cleaned monitoring site locations

--- data/monitoring_data/clean_monitoring_sites.R
+++ data/monitoring_data/clean_monitoring_sites.R
@@ -23,10 +23,13 @@ mpas <- readRDS(file=file.path(basedir, "mpa_traits/processed/CA_mpa_metadata.Rd
 # Format data
 ################################################################################
 
+# Step 1. Format data initially
+#######################################
+
 # Format sites
-sites <- site_locations %>% 
+sites1 <- site_locations %>% 
   # Rename
-  rename(long_dd=lon, lat_dd=lat, habitat=group, site_type=mpa_designation, mpa=affiliated_mpa) %>% 
+  rename(long_dd=lon, lat_dd=lat, habitat=group, site_type=mpa_designation, mpa_orig=affiliated_mpa) %>% 
   # Format lat/long
   mutate(long_dd=as.numeric(long_dd)) %>% 
   # Format habitat
@@ -39,7 +42,7 @@ sites <- site_locations %>%
   # Format site type
   mutate(site_type=ifelse(site_type=="ref", "Reference", "MPA")) %>% 
   # Format MPA name
-  mutate(mpa=stringr::str_to_title(mpa),
+  mutate(mpa=stringr::str_to_title(mpa_orig),
          mpa=gsub("Smr", "SMR", mpa),
          mpa=gsub("Smca", "SMCA", mpa),
          mpa=recode(mpa, 
@@ -61,21 +64,58 @@ sites <- site_locations %>%
                     # "Trinidad SMR"=""
                     "Swamis SMCA"="Swami's SMCA"),
          mpa=ifelse(mpa=="", NA, mpa)) %>%
-  # Fill in missing site names
-  # mutate(site_temp=make.unique(paste(mpa, site_type, sep="-"), sep="-")) %>% 
-  # mutate(site=ifelse(is.na(site), site_temp, site)) %>% 
-  # Remove unimportant columns
-  select(-c(mpa_class, site_temp))
-
-
-#   
-# centroid of the deep reef polygons - site name becomes easy
-
+  # Arrange
+  select(-mpa_class) %>% 
+  select(habitat, mpa, mpa_orig, site, site_type, everything())
 
 # Inspect MPA names
-mpas_in_data <- sort(unique(sites$mpa))  
+mpas_in_data <- sort(unique(sites1$mpa))  
 mpas_in_data[!mpas_in_data %in% mpas$mpa]
 
+# Inspect
+freeR::complete(sites1)
+
+# Are site ids unique?
+freeR::which_duplicated(sites1$site)
+
+
+# Step 2. Format deep reef and other
+#######################################
+
+# Format deep reef
+sites_deep <- sites1 %>% 
+  # Reduce to deep reef
+  filter(habitat=="Deep reef") %>%
+  # Average by site
+  group_by(habitat, mpa, mpa_orig, site_type) %>% 
+  summarize(lat_dd=mean(lat_dd),
+            long_dd=mean(long_dd)) %>% 
+  ungroup() %>% 
+  # Add site name
+  mutate(site=paste(habitat, mpa, site_type, sep="-")) %>% 
+  # Arrange
+  select(habitat, mpa, mpa_orig, site, site_type, everything())
+
+# Format other
+sites_other <- sites1 %>% 
+  # Reduce to deep reef
+  filter(habitat!="Deep reef") %>% 
+  # Fill in  missing site names
+  mutate(site_temp=make.unique(paste(habitat, mpa, site_type, sep="-"), sep="-")) %>%
+  mutate(site=ifelse(is.na(site), site_temp, site))
+
+
+# Step 3. Merge and format again
+#######################################
+
+# Merge
+sites <- bind_rows(sites_other, sites_deep) %>% 
+  # Average coordinates of duplicated sites: "LECHUZA", "POINT_ARENA_REFERENCE_3"
+  group_by(habitat, mpa, mpa_orig, site, site_type) %>% 
+  summarize(lat_dd=mean(lat_dd),
+            long_dd=mean(long_dd)) %>% 
+  ungroup()
+  
 # Inspect
 freeR::complete(sites)
 
@@ -84,3 +124,8 @@ freeR::which_duplicated(sites$site)
 
 # Export data
 saveRDS(sites, file.path(inputdir, "monitoring_sites_clean.Rds"))
+
+
+
+
+
