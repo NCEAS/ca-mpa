7f99e3ee8a3ed048524f2b21180984a56cda66d1
Chris Free <cfree14@gmail.com>
2022-08-10 10:19:20 -0700

    CUTI/BEUTI for every ecological monitoring site

--- data/environmental/Step3_calculate_environmental_data_at_sites.R
+++ data/environmental/Step3_calculate_environmental_data_at_sites.R
@@ -22,9 +22,32 @@ sites <- readRDS(file.path(basedir, "monitoring/monitoring_sites_clean.Rds"))
 upwelling <- readRDS(file.path(outputdir, "1988_2022_cuti_beuti_daily.Rds"))
 
 
-
-# Build time series
+# Build upwelling time series
 ################################################################################
 
+# Loop through sites and grab upwelling info
+x <- 1
+data_orig <- purrr::map_df(1:nrow(sites), function(x){
+  
+  # Subset row
+  site_info <- sites[x,]
+  site_lat <- site_info$lat_dd
+  site_lat_bin <- round( site_lat)
+  
+  # Build data
+  sdata <- filter(upwelling) %>% 
+    # Reduce to site lat bin
+    filter(lat_dd_bin==site_lat_bin ) %>% 
+    # Add site info
+    mutate(site=site_info$site, 
+           habitat=site_info$habitat,
+           mpa=site_info$mpa,
+           site_type=site_info$site_type) %>% 
+    # Arrange
+    select(habitat, site, site_type, mpa, everything())
+  
+})
 
+# Export data
+saveRDS(data_orig, file.path(outputdir, "1988_2022_cuti_beuti_daily_by_monitoring_site.Rds"))
 
