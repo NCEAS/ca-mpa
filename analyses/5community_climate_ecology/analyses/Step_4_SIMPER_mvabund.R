858610effc3a6babe6c7f04719fdaf493ded1483
Joshua Smith <joshsmith@nceas.ucsb.edu>
2022-08-31 12:05:00 -0700

    figure adjustments

--- analyses/5community_climate_ecology/analyses/Step_4_SIMPER_mvabund.R
+++ analyses/5community_climate_ecology/analyses/Step_4_SIMPER_mvabund.R
@@ -24,19 +24,28 @@ comm_data <- load(file.path(data_path, "comm_data.rda"))
 group_vars <- load(file.path(data_path, "group_vars.rda"))
 
 
-# run SIMPER tables before & after MHW------------------------------------------
 
+# run SIMPER tables before & after MHW------------------------------------------
 
 sim_CCFRP <- with(CCFRP_group_vars, simper(CCFRP_ord_data, MHW), 
                   ordered=TRUE)
-sim_kelp_swath <- with(kelp_swath_group_vars, simper(kelp_swath_ord_data, MHW),
+
+kelp_invalg_ord_data[is.na(kelp_invalg_ord_data)] = 0
+sim_kelp_invalg <- with(kelp_invalg_group_vars, simper(kelp_invalg_ord_data, MHW),
                   ordered=TRUE)
+
 sim_kelp_upc <- with(kelp_upc_group_vars, simper(kelp_upc_ord_data, MHW),
-                  ordered=TRUE)
+                 ordered=TRUE)
+
+sim_kelp_swath <- with(kelp_swath_group_vars, simper(kelp_swath_ord_data, MHW),
+                     ordered=TRUE)
+
 sim_kelp_fish <- with(kelp_fish_group_vars, simper(kelp_fish_ord_data, MHW),
                   ordered=TRUE)
+
 sim_deep_reef <- with(deep_reef_group_vars, simper(deep_reef_ord_data, MHW),
                   ordered=TRUE)
+
 sim_rocky <- with(rocky_group_vars, simper(rocky_ord_data, MHW),
                   oredered=TRUE)
 
@@ -51,11 +60,17 @@ CCFRP_a_b_table <- as.data.frame(summary(sim_CCFRP)$before_after)%>%
 kelp_swath_a_b_table <- as.data.frame(summary(sim_kelp_swath)$before_after)%>%
   mutate(group="kelp_swath",
          contrib = cumsum-lag(cumsum, default=0),
-         perc_change = (avb-ava)/ava,
-         sign = ifelse(perc_change > 0, "positive","negative"))
+        perc_change = (avb-ava)/ava,
+        sign = ifelse(perc_change > 0, "positive","negative"))
 
 kelp_upc_a_b_table <- as.data.frame(summary(sim_kelp_upc)$before_after)%>%
   mutate(group="kelp_upc",
+         contrib = cumsum-lag(cumsum, default=0),
+         perc_change = (avb-ava)/ava,
+         sign = ifelse(perc_change > 0, "positive","negative"))
+
+kelp_invalg_a_b_table <- as.data.frame(summary(sim_kelp_invalg)$before_after)%>%
+  mutate(group="kelp_invalg",
          contrib = cumsum-lag(cumsum, default=0),
          perc_change = (avb-ava)/ava,
          sign = ifelse(perc_change > 0, "positive","negative"))
@@ -79,8 +94,11 @@ rocky_a_b_table <- as.data.frame(summary(sim_rocky)$before_after)%>%
          sign = ifelse(perc_change > 0, "positive","negative"))
 
 
-simper_a_b_table <- rbind(CCFRP_a_b_table, kelp_swath_a_b_table, 
-                          kelp_upc_a_b_table, kelp_fish_a_b_table, 
+simper_a_b_table <- rbind(CCFRP_a_b_table, 
+                          #kelp_invalg_a_b_table,
+                          kelp_swath_a_b_table,
+                          kelp_upc_a_b_table,
+                          kelp_fish_a_b_table, 
                           deep_reef_a_b_table, rocky_a_b_table)%>%
   tibble::rownames_to_column(var = "species") %>%
   filter(cumsum <0.80)%>%
@@ -91,21 +109,108 @@ simper_a_b_table <- rbind(CCFRP_a_b_table, kelp_swath_a_b_table,
 
 # Examine output ----------------------------------------------------------
 
+# Clean up spp names 
+simper_a_b_table$species <- gsub('[[:digit:]]+', '', simper_a_b_table$species)
+
+#load KF taxon table
+taxon_tab <- readxl::read_excel(
+  file.path("/home/shares/ca-mpa/data/sync-data/monitoring/taxonomy_tables","Kelp-Taxonomy.xlsx"), sheet=1, skip = 0, na="NA")
+
+taxon_short <- taxon_tab %>%
+  select(common_name, ScientificName_accepted) %>%
+  mutate(join_ID = sub(" ","_", ScientificName_accepted))
 
 
-#set order
-simper_a_b_table$group <- factor(simper_a_b_table$group,
-                                 levels = c('CCFRP', 'kelp_swath', 
-                                            'rocky','kelp_fish',
-                                            'kelp_upc','deep_reef'))
+taxon_short$join_ID <- gsub("/","_",taxon_short$join_ID)
 
-ggplot(simper_a_b_table, aes(x = group, y = contrib, label = species, fill=sign)) +
+taxon_short$join_ID <- tolower(taxon_short$join_ID)
+
+
+simper_table <- left_join(simper_a_b_table, taxon_short, by=c("species"="join_ID"))%>%
+  mutate(species_ID = ifelse(is.na(common_name),species, common_name))%>%
+  filter(!(species_ID == 'chain-bladder kelp'))%>%
+  distinct()
+
+simper_table$species_ID <- recode_factor(simper_table$species_ID,
+                                         "sebastes_mystinus_or_diaconus"=
+                                           "blue rockfish")
+
+simper_table$species_ID <- gsub("_"," ",simper_table$species_ID)
+simper_table$species_ID <- gsub("Adult"," ",simper_table$species_ID)
+
+
+simper_table$species_ID <- str_to_title(simper_table$species_ID)
+
+simper_table <- simper_table %>%
+                mutate(monitoring = ifelse(group == 'kelp_upc'|
+                                             group== 'kelp_swath'|
+                                             group=='kelp_fish',
+                                           'Kelp Forest',
+                                           as.character(group)))
+simper_table$monitoring <- recode_factor(simper_table$monitoring,
+                                         'deep_reef'='Deep Reef')
+simper_table$monitoring <- recode_factor(simper_table$monitoring,
+                                         'rocky'='Rocky Intertidal')
+
+
+#add monitoring group level
+
+
+#rename
+
+simper_table$group <- recode_factor(simper_table$group, 'kelp_swath'=
+                                      'kelp and mobile inverts')
+
+simper_table$group <- recode_factor(simper_table$group, 'kelp_upc'=
+                                      'sessile inverts and algae')
+
+simper_table$group <- recode_factor(simper_table$group, 'kelp_fish'=
+                                      'Fish')
+
+simper_table$group <- recode_factor(simper_table$group, 'deep_reef'=
+                                      'Fish')
+
+simper_table$group <- recode_factor(simper_table$group, 'CCFRP'=
+                                      'Fish')
+
+simper_table$group <- recode_factor(simper_table$group, 'rocky'=
+                                      'Inverts and algae')
+
+
+
+
+simper_table$monitoring <- factor(simper_table$monitoring,
+                             levels = c('CCFRP', #'kelp_invalg', 
+                                        'Deep Reef',
+                                        'Kelp Forest',
+                                        'Rocky Intertidal'))
+
+
+#plot
+ggplot(simper_table, aes(x = group, y = contrib, label = species_ID, fill=sign)) +
   geom_bar(stat = "identity", color="black") +
   geom_text(size = 3, position = position_stack(vjust = 0.5), color="black", 
             fontface="bold")+
   theme_minimal(base_size = 21)+
   scale_fill_discrete(name = "direction of change")+
-  ylab("cumulative contribution")
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+  facet_grid(.~monitoring, scales = "free", switch = "x", space = "free_x") + 
+  theme(strip.placement = "outside")+
+  xlab("Monitoring Group")+
+  ylab("Cumulative Contribution (top 80%)")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
