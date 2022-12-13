f7c14a11e64b42fa80c4ef68bdb4c3b0cf96bdc2
Joshua Smith <joshsmith@nceas.ucsb.edu>
2022-12-13 13:32:46 -0800

    statewide logistic regression

--- analyses/5community_climate_ecology/analyses/Step_10_statewide_logistic_regression.R
+++ analyses/5community_climate_ecology/analyses/Step_10_statewide_logistic_regression.R
@@ -26,7 +26,7 @@ mod_out_raw <- read.csv(file.path(datadir,"mpa_betadisp_mod.csv"))
 
 trait_drivers <- mpa_trait %>%
                  dplyr::select(name, long, lat, implementation_date, size_km2, shore_span_km,
-                      max_depth_m, protection, depth_range)
+                      max_depth_m, protection, depth_range, four_region)
 
 
 mpa_dat <- left_join(mod_out_raw, trait_drivers, by=c("MPA"="name"))%>%
@@ -36,7 +36,13 @@ mpa_dat <- left_join(mod_out_raw, trait_drivers, by=c("MPA"="name"))%>%
             mutate(period=paste(period_1, period_2, sep="-"),
             process=recode_factor(period,
                                "before-during"="Resistance",
-                               "before-after"="Resilience"))
+                               "before-after"="Resilience"))%>%
+            #remove ref sites, since we don't know anything about their traits
+            filter(!(MPA_type == "ref"))%>%
+            dplyr::select(!(MPA_type))%>%
+            #calculate relative age
+            mutate(imp_year = format(as.Date(implementation_date, format="%m/%d/%Y"),"%Y"),
+                   rel_age = 2022 - as.numeric(imp_year))
 
 
 
@@ -46,67 +52,41 @@ mpa_dat <- left_join(mod_out_raw, trait_drivers, by=c("MPA"="name"))%>%
 in_resistance <- mpa_dat %>%
         mutate(logit_y = as.numeric(ifelse(stable == "yes","1","0")))%>%
         drop_na()%>%
-        dplyr::select(c(logit_y, process, habitat, MPA, MPA_type, size_km2, shore_span_km,
-               max_depth_m, depth_range))%>%
+        dplyr::select(c(logit_y, process, habitat, MPA, size_km2, shore_span_km,
+               max_depth_m, depth_range, rel_age, lat))%>%
         filter(process=="Resistance")%>%
         dplyr::select(!(process))
 
 in_resilience <- mpa_dat %>%
   mutate(logit_y = as.numeric(ifelse(stable == "yes","1","0")))%>%
   drop_na()%>%
-  dplyr::select(c(logit_y, process, habitat, MPA, MPA_type, size_km2, shore_span_km,
-                  max_depth_m, depth_range))%>%
-  filter(process=="Resilience",
-         MPA_type == "smr")%>%
+  dplyr::select(c(logit_y, process, habitat, MPA,  size_km2, shore_span_km,
+                  max_depth_m, depth_range, rel_age, lat, four_region))%>%
+  filter(process=="Resilience")%>%
   dplyr::select(!(process))
 
 
-#Do MPAs confer resilience across habitats? 
-resil_mod <- glm(as.numeric(logit_y) ~ habitat*MPA_type, data = in_resilience, 
-                     family=binomial(link="logit"), na.action = na.exclude)
-summary(resil_mod)
-
 #What is the effect of traits on resilience? 
-resil_mod <- glm(as.numeric(logit_y) ~ habitat + shore_span_km +
-                 size_km2, data = in_resilience, 
+resil_mod <- glm(as.numeric(logit_y) ~ four_region, data = in_resilience, 
                  family=binomial(link="logit"), na.action = na.exclude)
 summary(resil_mod)
 
 
+#What is the effect of traits on resistance? 
+resil_mod <- glm(as.numeric(logit_y) ~ habitat*shore_span_km +
+                   habitat*size_km2 + habitat*depth_range +
+                   habitat*rel_age, data = in_resistance, 
+                 family=binomial(link="logit"), na.action = na.exclude)
+summary(resil_mod)
 
 # plot predicted probabilities o
 
-pred_size <- ggpredict(resil_mod,terms = "size_km2[all]")%>%
+pred_age <- ggpredict(resil_mod,terms = "rel_age[all]")%>%
   ggplot(aes(x, predicted)) +
   geom_line() +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
   theme_minimal(base_size = 8)+
-  xlab("MPA size (Km2)")+
+  xlab("MPA age")+
   ylab("Probability of charismatic MPA")
-
-
-
-
-pred_beach_char <- ggpredict(char_step_mod,terms = "sandy_beach_km[all]")%>%
-  ggplot(aes(x, predicted)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
-  theme_minimal(base_size = 8)+
-  xlab("Sandy beach extent (km)")+
-  ylab("Probability of charismatic MPA")
-
-pred_park_char <- ggpredict(char_step_mod,terms = "nparks[all]")%>%
-  ggplot(aes(x, predicted)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
-  theme_minimal(base_size = 8)+
-  xlab("Park density (no. within 1km)")+
-  ylab("Probability of charismatic MPA")
-
-
-
-
-
-
 
 
