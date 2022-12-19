a4bbde3d3f28ce8b684ee9e0fff64638ada3db6d
Julien Brun <brun@nceas.ucsb.edu>
2022-12-19 05:09:21 -0800

    Computing wight from length

--- analyses/1performance_eco/code/biomass_reference_file_prep.R
+++ analyses/1performance_eco/code/biomass_reference_file_prep.R
@@ -9,38 +9,42 @@
 # install.packages(librarian)
 librarian::shelf(tidyverse, readxl)
 
-# Set the path names
+# Set the path and file names
 data_dir <- "/home/shares/ca-mpa/data/sync-data/species_traits/raw"
 kelp_file <- "kelp_forest_biomass_params.xlsx"
 ccfrp_file_old <- "old/CCFRP_biomass_params.csv"         # file we got through Shelby
 ccfrp_file <- "CCFRP_Biomass_Conversion_20220206.xlsx"   # file sent to us by Rachel Brooks, rachel.brooks@sjsu.edu, on 11/30/2022
 
 
-# Read the data in
+#### Read the data in ####
+
+# Kelp data
 raw_data_kf <- read_xlsx(file.path(data_dir, kelp_file), sheet = "species_attribute_table") %>%
   mutate(source = "kelp")
 
+# old CCFRP (Shelby)
 raw_data_ccfrp_old <- read_csv(file.path(data_dir, ccfrp_file_old)) %>%
   mutate(source = "ccfrp")
 
-raw_data_ccfrp <- read_xlsx(file.path(data_dir, ccfrp_file), sheet = "CCFRP_Biomass_Conversion_Table_") %>%
+# Lastest CCFRP (Rachel)
+raw_parameters_ccfrp <- read_xlsx(file.path(data_dir, ccfrp_file), sheet = "CCFRP_Biomass_Conversion_Table_") %>%
   mutate(source = "ccfrp") %>% 
   rename(units = ...16)  # fix column with no header in original file
 
-# all_equal(raw_data_ccfrp_old, raw_data_ccfrp)
 
-# filter for fish on keep columns of interest
-fish_data_kf <- raw_data_kf %>% 
-  filter(group == "fish") %>%
-  select(data_type, group, pisco_classcode, genus, species, common_name, ScientificName_accepted, 
-         juv_size_cutoff, contains("WL"), contains("LC"), Max_Length_fishbase, Size_Mature_cm) %>%
-  mutate(source = "kf")
+# Create a fake length data set  -- TEMPORARY to be replaced by real data
+length_data <- tibble(
+  species_scientificname = sample(na.omit(raw_data_ccfrp$ScientificName_accepted), 100, replace = TRUE),
+  TL_cm = rnorm(100, mean = 20, sd = 5)
+  )
 
-# Check for sie difference
-raw_diff <- anti_join( raw_data_ccfrp, fish_data_kf) 
 
-raw_all <- full_join(raw_data_ccfrp, fish_data_kf)
 
+
+
+#### DATA CONVERSION #####
+
+# Data conversion
 # We have several main cases:
 #  - We have the total length (TL):
 #     - We have the right unit for a and b: g and cm => done
@@ -50,11 +54,23 @@ raw_all <- full_join(raw_data_ccfrp, fish_data_kf)
 # 
 # Main formula: W = a * L^b
 
-# Unit transformation:
+# Unit transformation (source: Fishbase
 # a'(cm, g) = a (mm, g)*10^b
 # a'(cm, g) = a (cm, kg)*1000
 # a'(cm, g) = a (mm, mg)*10^b/1000
 # a'(cm, g) = a (mm, kg)*10^b*1000
+
+# Create a tibble from that
+a_prime_conversion <- tribble(
+  ~unit_length, ~unit_weight,  ~a_coeff, ~b_coeff,
+  "mm", "g",  10, 1,
+  "cm", "kg", 1000, NA,
+  "mm", "mg", 10, 1/1000,
+  "mm", "kg", 10, 1000
+)
+
+
+#### Length conversion ####
 
 
 #  Code from Shelby
@@ -82,38 +98,82 @@ raw_all <- full_join(raw_data_ccfrp, fish_data_kf)
 #   droplevels()
 
 
-## CCFRP
+# filter the necessary parameters
+conversion_parameters <- raw_parameters_ccfrp %>%
+  filter(!(is.na(WL_a))) %>%
+  filter(ScientificName_accepted %in% unique(length_data$species_scientificname))
 
-#  Subset the ready to go data (#20)
-ready_data_ccfrp <-  raw_data_ccfrp %>%
-  filter(WL_input_length == "TL") %>%
-  filter(WL_W_units == "g" & WL_L_units == "cm")
+# Join the parameters to the data (might be expensive with large data sets)
+data_ccfrg_param <- length_data %>% 
+  left_join(conversion_parameters, by = c("species_scientificname"="ScientificName_accepted"))
+
+# Compute from the cm inputs the type of length and units needed by the formula 
+data_length_ccfrg <-  data_ccfrg_param %>%
+  mutate(length_to_use = case_when(
+    WL_input_length == "TL" & WL_L_units == "cm" ~ TL_cm,
+    WL_input_length == "TL" & WL_L_units == "mm" ~ TL_cm *10,
+    WL_input_length == "SL" & WL_L_units == "cm" ~ (TL_cm - LC_b)/LC_a,
+    WL_input_length == "SL" & WL_L_units == "mm" ~ (TL_cm - LC_b)/LC_a *10,
+    WL_input_length == "FL" & WL_L_units == "cm" ~ TL_cm,
+    WL_input_length == "FL" & WL_L_units == "mm" ~ TL_cm *10,
+    WL_input_length == "DW" & WL_L_units == "cm" ~ TL_cm,
+    WL_input_length == "DW" & WL_L_units == "mm" ~ TL_cm *10,
+    TRUE ~ NA   #-9999 # for debugging, to be switched to NA
+  ),
+  # Compute the weight
+  weight_g = case_when(
+    WL_W_units == "g" ~ WL_a*length_to_use^WL_b,
+    WL_W_units == "kg" ~ (WL_a*length_to_use^WL_b)/1000,
+    TRUE ~ NA #-9999 # for debugging, to be switched to NA
+  )
+)
 
 
 
 
 
 
-## KELP
-
-#  Subset the ready to go data (#34)
-ready_data <- fish_data_kf %>%
-  filter(WL_input_length == "TL") %>%
-  filter(WL_W_units == "g" & WL_L_units == "cm")
-
-#  Subset the with a different length
-notTL_data <- fish_data %>%
-  filter(!(WL_input_length == "TL")) %>%
-  filter(WL_W_units == "g" & WL_L_units == "cm")
-
-unique(notTL_data$LL_Equation_for_WL)
 
 
-# Data with not transformation provided
-notTL_data_no_formula <- notTL_data %>%
-  filter(is.na(LL_Equation_for_WL))
 
 
+
+
+
+#### KELP ####
+
+# #  Subset the ready to go data (#34)
+# ready_data <- fish_data_kf %>%
+#   filter(WL_input_length == "TL") %>%
+#   filter(WL_W_units == "g" & WL_L_units == "cm")
+# 
+# #  Subset the with a different length
+# notTL_data <- fish_data %>%
+#   filter(!(WL_input_length == "TL")) %>%
+#   filter(WL_W_units == "g" & WL_L_units == "cm")
+# 
+# unique(notTL_data$LL_Equation_for_WL)
+# 
+# 
+# # Data with not transformation provided
+# notTL_data_no_formula <- notTL_data %>%
+#   filter(is.na(LL_Equation_for_WL))
+
+# # Quick comparison checks
+# 
+# # all_equal(raw_data_ccfrp_old, raw_data_ccfrp)
+# 
+# # filter for fish on keep columns of interest
+# fish_data_kf <- raw_data_kf %>% 
+#   filter(group == "fish") %>%
+#   select(data_type, group, pisco_classcode, genus, species, common_name, ScientificName_accepted, 
+#          juv_size_cutoff, contains("WL"), contains("LC"), Max_Length_fishbase, Size_Mature_cm) %>%
+#   mutate(source = "kf")
+# 
+# # Check for sie difference
+# raw_diff <- anti_join( raw_data_ccfrp, fish_data_kf) 
+# 
+# raw_all <- full_join(raw_data_ccfrp, fish_data_kf)
 
 
 
