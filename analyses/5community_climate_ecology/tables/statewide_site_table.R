a27feb859bf7686e0d00cb9a902c75b342c070e9
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-01-06 09:45:14 -0800

    working

--- analyses/5community_climate_ecology/tables/statewide_site_table.R
+++ analyses/5community_climate_ecology/tables/statewide_site_table.R
@@ -18,7 +18,7 @@ intertidal <- read.csv(file.path(datadir, "rocky_statewide.csv"))
 CCFRP <- read.csv(file.path(datadir, "CCFRP_statewide.csv"))
 deep_reef <- read.csv(file.path(datadir, "deep_reef_statewide.csv"))
 mpa_attributes_gen <- readRDS("/home/shares/ca-mpa/data/sync-data/mpa_traits/processed/mpa_attributes_general.Rds")%>%
-                      dplyr::select(affiliated_mpa, latitude_centroid, longitude_centroid)
+                      dplyr::select(affiliated_mpa, latitude_centroid, longitude_centroid, mpa_class)
 
 ################################################################################
 #Process and select variables of interest
@@ -59,7 +59,7 @@ site_dat <- rbind(kelp_data1, intertidal1, CCFRP1, deep_reef1)
 
 
 no_groups_yr <- site_dat %>%
-                group_by(year, region4, affiliated_mpa, mpa_designation)%>%
+                group_by(year, region3, affiliated_mpa, mpa_designation)%>%
                 summarize(total = sum(dummy_var))%>%
                 filter(!(mpa_designation == "smca"))%>%
                 mutate(affiliated_mpa = ifelse(affiliated_mpa =="ano nuevo smr",
@@ -69,21 +69,135 @@ no_groups_yr1 <- left_join(no_groups_yr, mpa_attributes_gen, by="affiliated_mpa"
 
 ################################################################################
 #generate plot
+# Theme
+theme1 <- theme(axis.text=element_text(size=5),
+                axis.text.y=element_text(size=4.5),
+                axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0),
+                axis.title=element_blank(),
+                #Legend
+                legend.text=element_text(size=5),
+                legend.title=element_text(size=6),
+                legend.key.size = unit(0.4, 'cm'), #change legend key size
+                legend.key.height = unit(0.4, 'cm'), #change legend key height
+                legend.key.width = unit(0.4, 'cm'), #change legend key width
+                legend.spacing.x = unit(0.2, 'cm'),
+                #strip.text=element_text(size=7),
+                #plot title
+                plot.title=element_text(size=10, face = "bold"),
+                # Gridlines
+                panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(), 
+                axis.line = element_line(colour = "black"),
+                # Legend
+                legend.background = element_rect(fill=alpha('blue', 0)))
 
-no_groups_yr1 %>%
+
+#filter data
+survey_effort <- no_groups_yr1 %>%
   filter(mpa_designation=="smr")%>%
-  mutate(region4 = factor(region4, levels=c("north","central","north islands","south")))%>%
-ggplot(aes(x = year, y = reorder(affiliated_mpa, latitude_centroid), fill=total)) +
-       geom_tile(color = "white",
+  mutate(region3 = factor(region3, levels=c("north","central","south")))
+
+north <- survey_effort %>%
+          filter(region3 == "north")%>%
+          ggplot(aes(x = year, y = reorder(affiliated_mpa, latitude_centroid), fill=total)) +
+          geom_tile(color = "white",
                  lwd=0.5,
                  linetype=1)+
-       facet_wrap(~region4, scales="free")+
-  scale_fill_gradient2(low = "#075AFF",
-                       mid = "#FFFFCC",
-                       high = "#FF0000") +
-  theme_classic()
+       scale_fill_gradient2(name="No. monitoring \ngroups",
+                            breaks=seq(0:4),
+                            labels=c(seq(0:4)),
+                          low="white", high="navy", na.value="grey50")+
+       scale_x_continuous(breaks = seq(2007,2020,1))+
+       geom_vline(xintercept = 2013.5, linetype = "dashed")+
+       geom_vline(xintercept = 2016.5, linetype = "dashed")+
+       
+       ggtitle("North")+
+       theme_bw() + theme1 +
+       theme(legend.position = "none")+
+  coord_fixed()
 
-no_groups_yr1$latitude_centroid
+north
+
+central <- survey_effort %>%
+  filter(region3 == "central")%>%
+  ggplot(aes(x = year, y = reorder(affiliated_mpa, latitude_centroid), fill=total)) +
+  geom_tile(color = "white",
+            lwd=0.5,
+            linetype=1)+
+  scale_fill_gradient2(name="No. monitoring \ngroups",
+                       breaks=seq(0:4),
+                       labels=c(seq(0:4)),
+                       low="white", high="navy", na.value="grey50")+
+  scale_x_continuous(breaks = seq(2007,2020,1))+
+  geom_vline(xintercept = 2013.5, linetype = "dashed")+
+  geom_vline(xintercept = 2016.5, linetype = "dashed")+
+  
+  ggtitle("Central")+
+  theme_bw() + theme1 +
+  theme(legend.position = "none")+
+  coord_fixed()
+
+central
+
+
+islands <- survey_effort %>%
+  filter(region3 == "north islands")%>%
+  ggplot(aes(x = year, y = reorder(affiliated_mpa, latitude_centroid), fill=total)) +
+  geom_tile(color = "white",
+            lwd=0.5,
+            linetype=1)+
+  scale_fill_gradient2(name="No. monitoring \ngroups",
+                       breaks=seq(0:4),
+                       labels=c(seq(0:4)),
+                       low="white", high="navy", na.value="grey50")+
+  scale_x_continuous(breaks = seq(2007,2020,1))+
+  geom_vline(xintercept = 2013.5, linetype = "dashed")+
+  geom_vline(xintercept = 2016.5, linetype = "dashed")+
+  
+  ggtitle("North Channel Islands")+
+  theme_bw() + theme1 +
+  theme(legend.position = "none")+
+  coord_fixed()
+
+islands
+
+
+south <- survey_effort %>%
+  filter(region3 == "south")%>%
+  ggplot(aes(x = year, y = reorder(affiliated_mpa, latitude_centroid), fill=total)) +
+  geom_tile(color = "white",
+            lwd=0.5,
+            linetype=1)+
+  scale_fill_gradient2(name="No. monitoring \ngroups",
+                       breaks=seq(0:4),
+                       labels=c(seq(0:4)),
+                       low="white", high="navy", na.value="grey50",
+                       limits = range(1,4))+
+  scale_x_continuous(breaks = seq(2007,2020,1))+
+  geom_vline(xintercept = 2013.5, linetype = "dashed")+
+  geom_vline(xintercept = 2016.5, linetype = "dashed")+
+  
+  ggtitle("South")+
+  theme_bw() + theme1 +
+  theme(plot.margin = margin(t=4,1,1,1,"lines"),
+        legend.position = c(0.2,1.2),
+        legend.direction = "horizontal")+
+  coord_fixed()
+
+south
+
+## Let ggarrange line up the panels
+g <- ggpubr::ggarrange(egg::ggarrange(north, central, ncol=1), 
+               south, ncol=2)
+
+
+# Export figure
+ggsave(g, filename=file.path(figdir, "FigSx_site_table.png"), 
+       width=6, height=7, units="in", dpi=600, bg="white")
+
+
+
 
 
 
