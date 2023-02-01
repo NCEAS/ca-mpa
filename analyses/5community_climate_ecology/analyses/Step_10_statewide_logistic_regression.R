49705648ffd99be2e0cc33f15d9d0be109a0d9ce
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-01-31 18:37:48 -0800

    working

--- analyses/5community_climate_ecology/analyses/Step_10_statewide_logistic_regression.R
+++ analyses/5community_climate_ecology/analyses/Step_10_statewide_logistic_regression.R
@@ -66,7 +66,8 @@ logit_dat <- left_join(mod_out1, mpa_traits, by=c("MPA"="affiliated_mpa")) %>%
             mutate(period=paste(period_1, period_2, sep="-"),
             process=recode_factor(period,
                                "before-during"="Resistance",
-                               "before-after"="Recovery"))
+                               "before-after"="Recovery"),
+            age = as.numeric(format(implementation_date,'%Y')))
             #
 
 
@@ -103,33 +104,53 @@ summary(recov_mod_all)
 ################################################################################
 # Build predicted probabilities
 
-# plot predicted probabilities o
+# Theme
+my_theme <-  theme(axis.text=element_text(size=7),
+                   axis.text.y = element_text(angle = 90, hjust = 0.5),
+                   axis.title=element_text(size=8),
+                   plot.tag=element_blank(), #element_text(size=8),
+                   plot.title =element_text(size=8, face="bold"),
+                   # Gridlines
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.key = element_blank(),
+                   legend.background = element_rect(fill=alpha('blue', 0)))
 
 pred_size <- ggpredict(recov_mod_all,terms = "size[all]") %>%
   ggplot(aes(x, predicted)) +
-  geom_smooth(span=0.25) +
+  geom_smooth(span=0.25, color='black') +
+  geom_point(aes(x=size, y=test_stat), data=recov_all)+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
+  annotate(geom="text", x=0, y=1.05, label="p=0.015", hjust=0, size=2.2) +
   theme_minimal(base_size = 12)+
   xlab("MPA size (km²)")+
-  ylab("Probability of recovery")
+  ylab("Probability of recovery")+
+  my_theme
 
 
 pred_rock <- ggpredict(recov_mod_all,terms = "prop_rock[all]") %>%
   ggplot(aes(x, predicted)) +
-  geom_smooth(span=0.25) +
+  geom_smooth(span=0.25, color='black') +
+  geom_point(aes(x=prop_rock, y=test_stat), data=recov_all)+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
+  annotate(geom="text", x=0, y=1.05, label="p=0.029", hjust=0, size=2.2) +
   theme_minimal(base_size = 12)+
-  xlab("Proportion rock")+
-  ylab("Probability of recovery")
+  xlab("Proportion rock habitat")+
+  ylab("")+
+  my_theme
 
-pred_hab_div <- ggpredict(recov_mod_all,terms = "habitat_diversity[all]") %>%
-  ggplot(aes(x, predicted)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
-  theme_minimal(base_size = 8)+
-  xlab("Habitat diversity")+
-  ylab("Probability of recovery")
 
+# Merge plots
+g <- gridExtra::grid.arrange(pred_size, pred_rock, nrow=1)
+g
+
+
+# Export
+ggsave(g, filename=file.path(figdir, "trait_drivers.png"), 
+       width=6.5, height=4.5, units="in", dpi=600)
 
 
 
