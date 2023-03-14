caa8eeee818499a7492ef362ba3899812599a183
cfree14 <cfree14@gmail.com>
2023-03-14 11:52:24 -0700

    figure work

---
+++ data/habitat_anita/Step1_clean_ci_data.R
@@ -0,0 +1,36 @@
+
+
+# Read data
+################################################################################
+
+# Clear workspace
+rm(list = ls())
+
+# Packages
+library(raster)
+library(terrainr)
+library(tidyverse)
+
+# Directories
+basedir <- "/Volumes/GoogleDrive/.shortcut-targets-by-id/1kCsF8rkm1yhpjh2_VMzf8ukSPf9d4tqO/MPA Network Assessment: Working Group Shared Folder/data/sync-data" #Chris
+indir <- file.path(basedir, "habitat_anita/raw")
+outdir <- file.path(basedir, "habitat_anita/processed")
+
+# Read full to use as template
+data_all <- raster::raster(file.path(basedir, "habitat_cdfw/processed/CA_bottom_substrate_10m.tiff"))
+
+# Read CI data
+# 1=hard substrate, 0=soft abstrate
+ci_orig <- raster::raster(file.path(indir, "CIN_rock/All_CI_N_rock_2m.tif"))
+
+
+# Project
+################################################################################
+
+# Reproject 
+ci <- raster::projectRaster(from=ci_orig, to=data_all, fun="mean", background=NA, na.rm=T)
+
+
+
+
+
