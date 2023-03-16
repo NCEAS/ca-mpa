9ffc8d88d67fceba99d8537eb605162d58d6f201
cfree14 <cfree14@gmail.com>
2023-03-15 22:00:27 -0700

    length weight params

--- data/species_traits/Step2_length_weight_params.R
+++ data/species_traits/Step2_length_weight_params.R
@@ -13,56 +13,153 @@ basedir <- "/Volumes/GoogleDrive/.shortcut-targets-by-id/1kCsF8rkm1yhpjh2_VMzf8u
 datadir <- file.path(basedir, "species_traits/processed")
 
 # Read data
-data_orig <- read.csv(file.path(datadir, "thermal_affinities_target_status (2).csv")) %>% 
-freeR::complete(data_orig)
+spp_orig <- read.csv(file.path(datadir, "full_taxon_table.csv"))
+freeR::complete(spp_orig)
 
-# Format data
+
+# Format taxa key
 ################################################################################
 
-# Species table
-spp_df <- data_orig %>% 
+# Format data
+spp <- spp_orig %>% 
   # Rename
-  janitor::clean_names("snake") %>% 
-  rename(pisco_code=pisco_classcode) %>% 
+  rename(species_id=common_name) %>% 
+  # Arrange
+  select(species_id, everything()) %>% 
+  # Format genus
+  mutate(genus=stringr::str_to_sentence(genus),
+         genus=ifelse(genus=="", NA, genus)) %>% 
+  # Add "spp" when missing
+  mutate(species=ifelse(is.na(species) & !is.na(genus), "spp", species)) %>% 
+  # Add scientific name
+  mutate(species_nwords=freeR::nwords(species),
+         sciname=ifelse(species_nwords==1, paste(genus, species), species)) %>% 
+  # Remove missing scientific names
+  mutate(sciname=ifelse(sciname=="NA NA", NA, sciname),
+         sciname=ifelse(sciname=="", NA, sciname)) %>% 
+  # Sentence case
+  mutate(sciname=sciname %>% stringr::str_squish() %>% stringr::str_to_sentence()) %>% 
   # Simplify
-  select(pisco_code, genus, species) %>% 
-  # Unique
+  select(species_id, genus, sciname) %>% 
   unique() %>% 
-  # Remove ones without genus
-  filter(genus!="") %>% 
-  # Sci name
-  mutate(sci_name_orig=paste(genus, species)) %>% 
-  select(pisco_code, genus, sci_name_orig) %>% 
-  mutate(sci_name_orig=recode(sci_name_orig,
-                              "Clupeiformes "="Clupeiformes spp",
-                              "Mytilus "="Mytilus spp")) %>% 
-  # Mark species or group
-  mutate(level=ifelse(grepl("spp|/|,", sci_name_orig), "group", "species")) %>% 
-  # Correct species names
-  mutate(sci_name=recode(sci_name_orig,
-                         # "Cribrinopsis albopunctata"=, 
-                         # "Diaperoforma californica"=,   
-                         # "Evasterias troschelii"=,        
-                         # "Neoagarum fimbriatum"=,        
-                         # "Neobernaya spadicea"=,          
-                         # "Pseudobatos productus"=,        
-                         # "Pugettia foliata"=,        
-                         # "Stephanocystis osmundacea"=,    
-                         # "Tetronarce californica"=
-                         "Sarda chiliensis chiliensis"="Sarda chiliensis"))
+  # Fix scientific name
+  mutate(sciname=recode(sciname,
+                        "<10 cm sebastes sp."="Sebastes spp",
+                        # "Actinostella bradleyi"                              
+                        # "Antiopella barbarensis"                              
+                        # "Atrimitra idae"                                     
+                        # "Beringraja binoculata"                               
+                        # "Beringraja stellulata"                              
+                        "Brevispinus, ochraceus, or giganteus"="Pisaster Brevispinus/ochraceus/giganteus",               
+                        # "Californiconus californicus"                        
+                        # "Californicus /syn./ parastichopus californicus"      
+                        "Californicus or pycnopodia helianthoides"="Rathbunaster californicus, Pycnopodia helianthoides",       
+                        "Carnatus, caurinus"="Sebastes carnatus/caurinus",
+                        "Ceramium flaccidum"="Gayliella flaccida",                              
+                        "Chiliensis chiliensis"="Sarda chiliensis",
+                        "Chrysomelas/carnatus young of year"="Sebastes chrysomelas/carnatus",               
+                        # "Coriacea /syn./ tealia coriacea"                     
+                        # "Cribrinopsis albopunctata"                          
+                        # "Cyanoplax hartwegii"                                 
+                        "Dawsoni or stimpsoni"="Solaster dawsoni/stimpsoni",
+                        # "Diaperoforma californica"                            
+                        # "Dictyoneurum californicum/reticulatum"              
+                        # "Dodecaceria fewkesi/concharum"                       
+                        # "Eopsetta  jordani"                                  
+                        # "Epitonium tinctum"                                   
+                        # "Evasterias troschelii"                              
+                        # "Felimare californiensis"                             
+                        # "Felimare porterae"                                  
+                        # "Felimida macfarlandi"                                
+                        # "Flabellinopsis iodinea"                             
+                        "Foliolata or astropecten armatus"="Luidia foliolata, Astropecten armatus",                
+                        # "Franciscanus /syn./ strongylocentrotus franciscanus"
+                        "Franciscanus or strongylocentrotus purpuratus"="Mesocentrotus franciscanus, Strongylocentrotus purpuratus",
+                        "Halichondria (halichondria)"="Halichondria panicea",
+                        "Haliclona (reniera)"="Haliclona cinerea",
+                        # "Hedophyllum sessile"                                
+                        "Holothuria (vaneyothuria) zacae"="Holothuria zacae",
+                        "Inflata or hippasteria spinosa"="Poraniopsis inflata, Hippasteria spinosa",
+                        # "Leukothele /syn./ parastichopus leukothele"          
+                        # "Lirobittium munitum"                                
+                        # "Lithopoma undosum"                                   
+                        # "Lofotensis /syn./ tealia lofotensis"                
+                        "Loligo opalescens"="Doryteuthis opalescens",                                   
+                        # "Lopholithodes mandtii/foraminatus"                  
+                        # "Loxorhynchus/scyra crispatus/acutifrons"             
+                        "Magister /syn./ cancer magister"="Metacarcinus magister",                    
+                        "Melanops / mystinus/ diaconus"="Sebastes melanops/mystinus/diaconus",                      
+                        "Melanops or mystinus"="Sebastes melanops/mystinus",                               
+                        # "Mexacanthina lugubris"                               
+                        "Miniata or mediaster aequalis"="Patiria miniata, Mediaster aequalis",
+                        "Mystinus or diaconus"="Sebastes mystinus/diaconus",
+                        # "Nemalion elminthoides"                              
+                        # "Neoagarum fimbriatum"                                
+                        # "Neobernaya spadicea"                                
+                        # "Neogastroclonium subarticulatum"                     
+                        # "Nobilis /syn./ anisodoris nobilis"                  
+                        # "Norrisia norrisii"                                   
+                        # "Okenia rosacea"                                     
+                        "Opalescens (eggs)"="Doryteuthis opalescens",
+                        # "Parvimensis /syn./ parastichopus parvimensis"       
+                        # "Peltodoris nobilis"                                  
+                        # "Phyllospadix scouleri"                              
+                        # "Phyllospadix serrulatus"                             
+                        # "Phyllospadix torreyi"                               
+                        "Pinniger or miniatus"="Sebastes pinniger/miniatus",                              
+                        # "Plocamium violaceum"                                
+                        # "Pseudobatos productus"                               
+                        # "Psolus chitonoides"                                 
+                        # "Pugettia foliata"                                    
+                        # "Ritteri /syn./ heteropolypus ritteri"               
+                        # "Scyra/oregonia acutifrons/gracilis"                  
+                        "Sebastes (10-15"="Sebastes spp",                                  
+                        "Sebastes atrovirens,carnatus,chrysomelas,caurinus"="Sebastes atrovirens/carnatus/chrysomelas/caurinus",   
+                        "Sebastes dalli"="Sebastes dallii",                                   
+                        # "Sebastes diaconus"                                   
+                        "Sebastes n/a"="Sebastes spp",                                        
+                        "Sebastes serranoides,flavidus"="Sebastes serranoides/flavidus",                         
+                        "Sebastes serranoides,flavidus,melanops"="Sebastes serranoides/flavidus/melanops",         
+                        "Serranoides or flavidus"="Sebastes serranoides/flavidus",                        
+                        # "Stephanocystis dioica"                              
+                        # "Stephanocystis osmundacea"                           
+                        # "Taonia lennebackerae"                               
+                        # "Tethya aurantia"                                     
+                        "Tetilla sp"="Tetilla spp",                                        
+                        # "Tetronarce californica"                              
+                        # "Thylacodes/petaloconchus squamigerus/montereyensis" 
+                        # "Trikentrion helium"                                  
+                        # "Urticina columbiana/mcpeaki"                        
+                        # "Xenistius californiensis"                            
+                        # "Xystreurys rubescens"
+                        )) %>% 
+  # Mark level
+  mutate(level=ifelse(grepl("spp|/|,", sciname) | is.na(genus), "group", "species"))
 
 # Inspect
-freeR::which_duplicated(spp_df$pisco_code)
+sort(unique(spp$genus))
+
 
 # Check names
-names2check <- spp_df$sci_name[spp_df$level=="species"]
-freeR::check_names(names2check)
+spp_names <- spp$sciname[spp$level=="species"] %>% unique() %>% sort()
+wrong_names <- freeR::check_names(spp_names)
+wrong_names 
 
-# Retrieve length weight
+# Export
+write.csv(spp, file=file.path(datadir, "species_key.csv"), row.names = F)
 
 
+# Get length weight data
+################################################################################
 
+# Retrieve taxa
+taxa <- freeR::taxa(species=spp_names)
 
+# Retrieve length-weight
+lw <- freeR::fishbase(dataset="lw", species=spp_names, level="family", cleaned=T, add_taxa = F)
+
+# Export
+write.csv(lw, file=file.path(datadir, "species_lw_parameters_from_fishbase_full.csv"), row.names = F)
 
 
 
