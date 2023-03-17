0747030ef3f2f080d43eb128c64c8f62fb82bc8a
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-03-17 08:50:03 -0700

    updated taxon tab

--- data/species_traits/step_2_gather_taxon_tables.R
+++ data/species_traits/step_2_gather_taxon_tables.R
@@ -9,75 +9,83 @@ export_path <- "/home/shares/ca-mpa/data/sync-data/species_traits/processed"
 ################################################################################
 #load taxonomy tables
 
-rocky_taxon <- readxl::read_excel(
-  file.path("/home/shares/ca-mpa/data/sync-data/monitoring/taxonomy_tables","RockyIntertidal-LongTerm-Taxonomy.xlsx"), sheet=1, skip = 0, na="NA")%>%
-  dplyr::select(marine_species_code, marine_species_name, Genus, Species)
-rocky_taxon$marine_species_code <- tolower(rocky_taxon$marine_species_code)
+rocky_taxon1 <- readxl::read_excel(
+  file.path("/home/shares/ca-mpa/data/sync-data/monitoring/taxonomy_tables","RockyIntertidal-LongTerm-Taxonomy.xlsx"), sheet=1, skip = 0, na="NA")
 
 
-data_path <- "/home/shares/ca-mpa/data/sync-data/monitoring/monitoring_kelp"
-input_file <- "MLPA_kelpforest_taxon_table.4.csv"  
-kelp_taxon <- read.csv(file.path(data_path, input_file)) %>%
-  janitor::clean_names() %>%
-  dplyr::select(classcode, species_definition, genus, species, common_name) %>%
-  distinct() #remove duplicates
-
-kelp_taxon$species_definition <- tolower(kelp_taxon$species_definition)
-kelp_taxon$common_name <- tolower(kelp_taxon$common_name)
+kelp_taxon1 <-readxl::read_excel(
+  file.path("/home/shares/ca-mpa/data/sync-data/monitoring/taxonomy_tables/Kelp-Taxonomy.xlsx"), sheet=1, skip = 0, na="NA")
 
 
-
-data_path <- "/home/shares/ca-mpa/data/sync-data/monitoring/monitoring_ccfrp/CCFRP_database/CCFRP_database_2007-2020_csv"
-input_file <- "fish_species.csv"  
-
-ccfrp_taxon <- read.csv(file.path(data_path, input_file)) %>%
-  janitor::clean_names() %>%
-  dplyr::select(common_name, genus, species) %>%
-  distinct() %>%#remove duplicates
-  mutate(species_definition = paste(genus, species))
-
-ccfrp_taxon$common_name <- tolower(ccfrp_taxon$common_name)
-ccfrp_taxon$species_definition <- tolower(ccfrp_taxon$species_definition)
+ccfrp_taxon1 <- readxl::read_excel(
+  file.path("/home/shares/ca-mpa/data/sync-data/monitoring/taxonomy_tables/CCFRP_Taxonomy.xlsx"), sheet=2, skip = 0, na="NA")
 
 
 data_path <- "/home/shares/ca-mpa/data/sync-data/monitoring/taxonomy_tables"
 input_file <- "DeepReef-ROV-Taxonomy.xlsx"  
-
-deep_reef_taxon <- readxl::read_excel(file.path(data_path,input_file),sheet = 1)%>%
+deep_reef_taxon1 <- readxl::read_excel(file.path(data_path,input_file),sheet = 1)%>%
   clean_names()
 
-deep_reef_taxon$common_name <- tolower(deep_reef_taxon$common_name)
-deep_reef_taxon$scientific_name <- tolower(deep_reef_taxon$scientific_name)
+
+
+################################################################################
+#clean tables
+
+#rocky intertidal
+rocky_taxon <- rocky_taxon1 %>%
+                mutate(habitat = "Rocky intertidal",
+                       target_status = NA)%>%
+                dplyr::select(habitat, habitat_specific_code = "marine_species_code", habitat_specific_spp_name = "marine_species_name",
+                              Kingdom, Phylum, Class, Order, Family, Genus, Species, target_status) 
+rocky_taxon[rocky_taxon == "NULL"] <- NA
+rocky_taxon <- rocky_taxon %>% mutate(Species = word(Species, -1))
+
+rocky_taxon$Phylum <-  gsub("[()]", "", rocky_taxon$Phylum)  
+rocky_taxon$Species <-  gsub("[()]", "", rocky_taxon$Species)  
+
+
+#kelp forest
+kelp_taxon <- kelp_taxon1 %>%
+              mutate(habitat = "Kelp forest")%>%
+                dplyr::select(habitat, habitat_specific_code = "pisco_classcode", habitat_specific_spp_name = "ScientificName_accepted",
+                Kingdom, Phylum, Class, Order, Family, Genus = genus, Species = species, target_status = Targeted) %>%
+              distinct()
+kelp_taxon[kelp_taxon == "spp"] <- NA
+kelp_taxon[kelp_taxon == "spp."] <- NA
+
+#CCFRP
+
+CCFRP_taxon <- ccfrp_taxon1%>%
+  mutate(habitat = "Rocky reef")%>%
+  dplyr::select(habitat, habitat_specific_code = "Species_Code", habitat_specific_spp_name = "Scientific_Name",
+                Kingdom, Phylum, Class, Order, Family, Genus, Species = species, target_status=Fished) %>%
+  distinct()
+
+#Deep reef
+
+deep_reef_taxon <- deep_reef_taxon1%>%
+  mutate(habitat = "Deep reef")%>%
+  dplyr::select(habitat, habitat_specific_code = "pisco_code", habitat_specific_spp_name = "scientific_name",
+                Kingdom = kingdom, Phylum = phylum, Class = class, Order = order, Family = family, 
+                Genus = genus, Species = species, target_status=targeted) %>%
+  distinct()
+deep_reef_taxon[deep_reef_taxon == "NA"] <- NA
 
 
 ################################################################################
 #merge
 
-
-rocky_merge <- rocky_taxon %>% dplyr::select(genus = Genus, species = Species, common_name = marine_species_name)
-
-kelp_merge <- kelp_taxon %>% dplyr::select(genus, species, common_name)
-
-ccfrp_merge <- ccfrp_taxon %>% dplyr::select(genus, species, common_name)  
-
-deep_reef_merge <- deep_reef_taxon %>% dplyr::select(genus, species, common_name)  
-
-
-taxon_tab <- rbind(rocky_merge, kelp_merge, ccfrp_merge, deep_reef_merge) 
-
-
-#clean ip
+taxon_tab <- rbind(rocky_taxon, kelp_taxon, CCFRP_taxon, deep_reef_taxon)
 taxon_tab[taxon_tab == "NA"] <- NA
-taxon_tab[taxon_tab == "NULL"] <- NA
-taxon_tab[taxon_tab == "spp"] <- NA
+taxon_tab[taxon_tab == "?"] <- NA
+taxon_tab[taxon_tab == "Non-targeted"] <- "Nontargeted"
 
 
-taxon_tab1 <- taxon_tab %>% mutate(genus = tolower(genus),
-                                   species = tolower(species))
+
 
 
 #export
-write.csv(taxon_tab1, file.path(export_path, "full_taxon_table.csv"), row.names = FALSE)
+write.csv(taxon_tab, file.path(export_path, "full_taxon_table_new.csv"), row.names = FALSE)
 
 
 
