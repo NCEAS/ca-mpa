afc98b82977bf0a843d4615c1018eb79067bb501
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-03-20 12:54:57 -0700

    claculate and process kelp forest fish biomass

--- data/monitoring_data/processing_code/biomass/Step1_process_biomass.R
+++ data/monitoring_data/processing_code/biomass/Step1_process_biomass.R
@@ -18,8 +18,33 @@ kelp_forest_raw <- read.csv(file.path(datadir, "/monitoring_kelp/MLPA_kelpforest
 #load taxonomy lookup table
 taxon_tab <- read.csv("/home/shares/ca-mpa/data/sync-data/species_traits/processed/species_key.csv")
 
+#load site table
+kelp_sites <- read.csv(file.path(datadir, "/monitoring_kelp/MLPA_kelpforest_site_table.4.csv")) %>%
+  janitor::clean_names() %>%
+  dplyr::select(site, ca_mpa_name_short, mpa_class=site_designation, mpa_designation=site_status)%>%
+  distinct() #remove duplicates
+
+#load habitat data
+mpa_attributes_gen <- readRDS("/home/shares/ca-mpa/data/sync-data/mpa_traits/processed/mpa_attributes_general.Rds")
+mpa_attributes_hab <- readRDS("/home/shares/ca-mpa/data/sync-data/mpa_traits/processed/mpa_attributes_habitat.Rds")
+mpa_attributes_hab_div <- readRDS("/home/shares/ca-mpa/data/sync-data/mpa_traits/processed/mpa_attributes_habitat_diversity.Rds")
+fishing_effort <- readRDS(here::here("analyses","2performance_fisheries","analyses","blocks","pre_mpa_fishing_pressure_by_mpa.Rds"))
+prop_rock <- readRDS("/home/shares/ca-mpa/data/sync-data/mpa_traits/processed/mpa_attributes_habitat_rock.Rds")
+
+regions <- mpa_attributes_gen %>%
+  dplyr::select(name, bioregion, region4 = four_region_north_ci) %>%
+  mutate(name = tolower(name))
+
+#load defacto SMRs
+data_path <- "/home/shares/ca-mpa/data/sync-data/mpa_traits"
+input_file <- "mpa-attributes.xlsx" 
+defacto_smr_kelp <- readxl::read_excel(file.path(data_path, input_file), sheet=5, skip = 0, na="NA")%>%
+  filter(group=="kelp") %>%
+  dplyr::select(affiliated_mpa, mpa_defacto_class = mpa_class)
+
 #load lw params
-params_tab <- read.csv("/home/shares/ca-mpa/data/sync-data/species_traits/processed/fish_lw_parameters_by_species.csv")
+params_tab <- read.csv("/home/shares/ca-mpa/data/sync-data/species_traits/processed/fish_lw_parameters_by_species.csv") %>%
+                mutate(ScientificName_accepted = recode(ScientificName_accepted, "Sebastes spp." = "Sebastes spp"))
 
 ################################################################################
 #prep conversion function
@@ -74,16 +99,77 @@ bio_fun <- function(data_with_params) {
 #process kelp forest
 
 kelp_code <- taxon_tab %>% filter(habitat=="Kelp forest")
-
 kelp_forest_process1 <- left_join(kelp_forest_raw, kelp_code, by=(c("classcode"="habitat_specific_code")))
 
 #estimate biomass
 
 kelp_dat <- convert_dat(params_tab, kelp_forest_process1)
-kelp_out <- bio_fun(kelp_dat)
+kelp_out <- bio_fun(kelp_dat) %>% 
+  #drop species with missing size
+  filter(!(is.na(TL_cm)))%>%
+  #drop observations not identified to species level
+  filter(!(
+  classcode == "NO_ORG" |
+    classcode == "UNID" | #unidentified fish
+    classcode == "ATHE" |
+    classcode == "UHAL" | #stingray
+    classcode == "TCAL" | #torpedo ray
+    classcode == "SYNG" | #tubesnout
+    classcode == "STICH" |
+    classcode == "RRIC" |
+    classcode == "RJOR" |
+    classcode =="RHYP" | #Ronquil
+    classcode =="RALL" |#Ronquil
+    classcode == "EMBI"|
+    classcode == "BOTH"|
+    classcode == "CITH" |
+    classcode == "PHOL" |
+    classcode =="PLEU"|
+    classcode == "CLUP" |
+    classcode == "BATH" |
+    classcode == "GGAL" |
+    classcode == "HEXA" |
+    classcode == "PPRO"
+    )) %>%
+  #calculate total biomass
+  mutate(total_biom_g = weight_g*count)%>%
+  #select interest vars
+  dplyr::select(year, month, day, site, zone, level = level.x, transect,
+                classcode, count, TL_cm, sciname, weight_g, total_biom_g, target_status)
+
+#add regions
+kelp_fish_counts <- left_join(kelp_out, kelp_sites, by="site") %>%
+  ungroup() %>%
+  dplyr::select(year, month, day, affiliated_mpa=ca_mpa_name_short,mpa_class, mpa_designation, everything()) %>%
+  mutate(mpa_designation = ifelse(mpa_designation=="reference","ref",mpa_class))
+
+kelp_fish_counts$affiliated_mpa <- tolower(kelp_fish_counts$affiliated_mpa)
+kelp_fish_counts <- left_join(kelp_fish_counts, regions, by=c("affiliated_mpa"="name"))
 
 
+#add defacto SMRs
+kelp_fish_counts <- left_join(kelp_fish_counts, defacto_smr_kelp, by="affiliated_mpa")
 
+#clean up
+
+kelp_fish_counts_final <- kelp_fish_counts %>% 
+                      dplyr::select(year, month, day, affiliated_mpa, 
+                                    mpa_state_class = mpa_class.x,
+                                    mpa_defacto_class, bioregion, region4,
+                                    everything()) %>%
+                      filter(!(is.na(mpa_defacto_class)))%>%
+                      mutate(mpa_defacto_class = tolower(mpa_defacto_class),
+                             mpa_designation = tolower(mpa_designation),
+                             mpa_defacto_designation = ifelse(mpa_defacto_class == "ref","ref",mpa_defacto_class),
+                             mpa_state_class = tolower(mpa_state_class))%>%
+                      dplyr::rename(mpa_state_designation = mpa_designation)%>%
+                      select(!(mpa_class.y)) %>%
+  dplyr::select(year, month, day, affiliated_mpa, 
+                mpa_state_class, mpa_state_designation,
+                mpa_defacto_class, mpa_defacto_designation, bioregion, region4,
+                everything()) 
+
+#write.csv(kelp_fish_counts_final, row.names = F, file.path(outdir,"/biomass_processed/kelpforest_fish_biomass.csv"))
 
 
 
