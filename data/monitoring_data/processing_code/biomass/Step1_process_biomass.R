7025b132de81549401da45c3846940026a2bb985
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-03-19 17:01:24 -0700

    wrapper function for biomass conversion

--- data/monitoring_data/processing_code/biomass/Step1_process_biomass.R
+++ data/monitoring_data/processing_code/biomass/Step1_process_biomass.R
@@ -18,8 +18,57 @@ kelp_forest_raw <- read.csv(file.path(datadir, "/monitoring_kelp/MLPA_kelpforest
 #load taxonomy lookup table
 taxon_tab <- read.csv("/home/shares/ca-mpa/data/sync-data/species_traits/processed/species_key.csv")
 
+#load lw params
+params_tab <- read.csv("/home/shares/ca-mpa/data/sync-data/species_traits/processed/fish_lw_parameters_by_species.csv")
+
 ################################################################################
-#process ccfrp biomass data
+#prep conversion function
+
+a_prime_conversion <- tribble(
+  ~unit_length, ~unit_weight,  ~a_coeff, ~b_coeff,
+  "mm", "g",  10, 1,
+  "cm", "kg", 1000, NA,
+  "mm", "mg", 10, 1/1000,
+  "mm", "kg", 10, 1000
+)
+
+
+# filter the necessary parameters
+
+convert_dat <- function(params, data){
+  
+  convert_params <- params %>%
+  filter(!(is.na(WL_a))) %>% # remove rows without conversion
+  filter(ScientificName_accepted %in% unique(data$sciname))
+
+  data %>% left_join(convert_params, by = c("sciname"="ScientificName_accepted"), multiple = "all")
+}
+
+
+# Compute from the cm inputs the type of length and units needed by the formula 
+bio_fun <- function(data_with_params) {
+  data_with_params %>%
+  rename(TL_cm = fish_tl)%>%
+  mutate(length_to_use = case_when(
+    WL_input_length == "TL" & WL_L_units == "cm" ~ TL_cm,
+    WL_input_length == "TL" & WL_L_units == "mm" ~ TL_cm *10,
+    WL_input_length == "SL" & WL_L_units == "cm" ~ (TL_cm - LC_b)/LC_a,
+    WL_input_length == "SL" & WL_L_units == "mm" ~ (TL_cm - LC_b)/LC_a *10,
+    WL_input_length == "FL" & WL_L_units == "cm" ~ TL_cm,
+    WL_input_length == "FL" & WL_L_units == "mm" ~ TL_cm *10,
+    WL_input_length == "DW" & WL_L_units == "cm" ~ TL_cm,
+    WL_input_length == "DW" & WL_L_units == "mm" ~ TL_cm *10,
+    TRUE ~ NA   #-9999 # for debugging, to be switched to NA
+  ),
+  # Compute the weight
+  weight_g = case_when(
+    WL_W_units == "g" ~ WL_a*length_to_use^WL_b,
+    WL_W_units == "kg" ~ (WL_a*length_to_use^WL_b)/1000,
+    TRUE ~ NA #-9999 # for debugging, to be switched to NA
+  )
+  ) 
+}
+
 
 ################################################################################
 #process kelp forest
@@ -28,6 +77,10 @@ kelp_code <- taxon_tab %>% filter(habitat=="Kelp forest")
 
 kelp_forest_process1 <- left_join(kelp_forest_raw, kelp_code, by=(c("classcode"="habitat_specific_code")))
 
+#estimate biomass
+
+kelp_dat <- convert_dat(params_tab, kelp_forest_process1)
+kelp_out <- bio_fun(kelp_dat)
 
 
 
@@ -37,7 +90,3 @@ kelp_forest_process1 <- left_join(kelp_forest_raw, kelp_code, by=(c("classcode"=
 
 
 
-
-
-
-                                  
