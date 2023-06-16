81e96ded69667c63f85097acdbaffdf936d4bb84
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-06-16 15:24:05 -0700

    absolute temps figure

--- analyses/5community_climate_ecology/analyses/FigSX_absolute_temperatures.R
+++ analyses/5community_climate_ecology/analyses/FigSX_absolute_temperatures.R
@@ -15,31 +15,33 @@ gisdir <- file.path(basedir, "gis_data/processed")
 plotdir <- "analyses/5community_climate_ecology/figures"
 
 # Read data
-envi_orig <- readRDS(file.path(basedir, "envr_anomalies_at_mpas_new.Rds")) # Josh
-monthly_temp <- readRDS(file.path(basedir, "monthly_air_temp_central_coast.rds")) # Josh
+envi_orig <- readRDS(file.path(basedir, "envr_anomalies_at_mpas_new.Rds")) 
+monthly_temp <- readRDS(file.path(basedir, "monthly_air_temp_central_coast.rds")) 
 
 ###############################################################################
 #Process data
+# Process data
 envi <- envi_orig %>%
   # Filter
-  filter(region3=='central' & year>=2000) %>%
+  filter(region3=='central' & year>=2002 & year <= 2020)%>%
   # Summarize by year and month
   group_by(year, month)%>%
-  summarise(beuti_anom = mean(beuti_monthly_anom, na.rm=T),
-            cuti_anom = mean(cuti_monthly_anom, na.rm=T),
-            sst_anom = mean(sst_monthly_anom, na.rm=T),
-            bottomT_anom = mean(as.numeric(as.character(bottomT_monthly_anom)),na.rm=T),
+  summarise(beuti_abs = mean(beuti_monthly_obs, na.rm=T),
+            cuti_abs = mean(cuti_monthly_obs, na.rm=T),
+            sst_abs = mean(sst_monthly_obs, na.rm=T),
+            bottomT_abs = mean(as.numeric(as.character(bottomT_monthly_obs)),na.rm=T),
             quarterly_MOCI = mean(quarterly_MOCI, na.rm=T))  %>% 
   ungroup() %>% 
   # Gather
   gather(key="indicator", value="value", 3:ncol(.)) %>% 
   # Format indicator name
   mutate(indicator=recode(indicator, 
-                          "beuti_anom"="BEUTI",
-                          "cuti_anom"="CUTI",
-                          "sst_anom"="SST",
-                          "bottomT_anom" = "Bottom temp",
-                          "quarterly_MOCI"="MOCI")) %>% 
+                          "beuti_abs"="BEUTI",
+                          "cuti_abs"="CUTI",
+                          "sst_abs"="SST",
+                          "bottomT_abs" = "Bottom temp",
+                          "quarterly_MOCI"="MOCI"
+                          )) %>% 
   # Summarize by year
   group_by(indicator, year) %>% 
   summarize(value_avg=mean(value),
@@ -53,89 +55,78 @@ envi <- envi_orig %>%
 
 envi_dat <- envi_orig %>%
   # Filter
-  filter(region3=='central' & year>=2000)%>%
+  filter(region3=='central' & year>=2002 & year <= 2020)%>%
   mutate( date_order = as.numeric(paste0(year,".",month))
           #Date = with(., sprintf("%d-%02d", year, month))
   ) %>%
-  filter(date_order <= 2020.5)
+  filter(date_order <= 2020.5) 
 
-
-###############################################################################
 #Process air temp
 # Convert "year" and "month" to Date format
 monthly_temp$year_month <- as.Date(paste0(monthly_temp$year, "-", monthly_temp$month, "-01"))
 
-# Calculate the mean and standard error for each year
 yearly_stats <- monthly_temp %>%
   group_by(year) %>%
   summarize(
-    mean_anomaly = mean(monthly_mean_anomaly),
-    se = sd(monthly_mean_anomaly) / sqrt(n())
-  )
+    mean_temp = mean(observed_monthly_mean),
+    se = sd(observed_monthly_mean) / sqrt(n()),
+    value_lo=mean_temp-1.96*se,
+    value_hi=mean_temp+1.96*se)
+
+#Date = with(., sprintf("%d-%02d", year, month))
 
 
 # Plot air temp anomalies
 ################################################################################
-my_theme <- theme(axis.text=element_text(size=8),
-                  #axis.text.y = element_text(angle = 90, hjust = 0.5),
-                  axis.title=element_text(size=10),
-                  plot.tag=element_blank(), #element_text(size=8),
-                  plot.title =element_text(size=10, face="bold"),
-                  # Gridlines
-                  panel.grid.major = element_line(colour = "transparent"), 
-                  panel.grid.minor = element_line(colour = "transparent"), 
-                  panel.background = element_blank(), 
-                  axis.line = element_line(colour = "black"),
-                  axis.ticks = element_line(colour = "black"),
-                  # Legend
-                  legend.key = element_blank(),
-                  legend.text=element_text(size=8),
-                  legend.title=element_text(size=10),
-                  # legend.background = element_rect(fill=alpha('blue', 0)),
-                  #facets
-                  strip.text = element_text(size=7, hjust=0, face="bold"),
-                  strip.background = element_blank()
-                  #margins
-                  #plot.margin=unit(c(0.01,0.01,0.01,0.01),"cm")
-)
-
-# Convert "year" and "month" to Date format
-monthly_temp$year_month <- as.Date(paste0(monthly_temp$year, "-", monthly_temp$month, "-01"))
-
-# Calculate the mean and standard error for each year
-yearly_stats <- monthly_temp %>%
-  group_by(year) %>%
-  summarize(
-    mean_anomaly = mean(monthly_mean_anomaly),
-    se = sd(monthly_mean_anomaly) / sqrt(n())
-  )
+# Theme
+base_theme <-  theme(axis.text=element_text(size=7,colour = "black"),
+                     axis.title=element_text(size=8,colour = "black"),
+                     legend.text=element_text(size=7,colour = "black"),
+                     legend.title=element_text(size=8,colour = "black"),
+                     plot.tag=element_text(size=8,colour = "black"),
+                     # Gridlines
+                     panel.grid.major = element_blank(), 
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(), 
+                     axis.line = element_line(colour = "black"),
+                     # Legend
+                     legend.key = element_rect(fill=alpha('blue', 0)),
+                     legend.background = element_rect(fill=alpha('blue', 0)))
 
 # Create the plot
-ymax <- max(monthly_temp$monthly_mean_anomaly, na.rm=TRUE) * .90
-
-g <- ggplot() +
-  geom_point(data = monthly_temp %>% filter(year >2006 & year < 2020), aes(x = year_month, y = monthly_mean_anomaly), alpha = 0.6, color='lightgray') +
-  geom_ribbon(data = yearly_stats %>% filter(year >2006 & year < 2021), aes(x = as.Date(paste0(year, "-01-01")), ymin = mean_anomaly - se, ymax = mean_anomaly + se), fill = "black", alpha = 0.3) +
-  geom_line(data = yearly_stats %>% filter(year >2006 & year < 2021), aes(x = as.Date(paste0(year, "-01-01")), y = mean_anomaly), color = "black", group = 1) +
-  annotate(geom="rect", xmin = as.Date("2014-01-01"), xmax = as.Date("2016-12-31"), ymin=-Inf, ymax=Inf, fill="red", alpha=0.2) +
-  annotate(geom="text", label="MHW", x= as.Date("2008-01-01"), y=ymax , size=2.5) +
-  annotate("segment", x = as.Date("2010-06-01"), y = ymax, xend = as.Date("2013-06-01"), yend = ymax,
-          arrow = arrow(type = "closed", length = unit(0.02, "npc")))+
-   labs(x = "Year", y = "Air temperature anamoly (°C)") +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+#AT
+ymax <- max(monthly_temp$observed_monthly_mean, na.rm = TRUE) 
+g1 <- ggplot() +
+  geom_point(data = monthly_temp %>% filter(year > 2002 & year < 2020),
+             aes(x = as.numeric(year) + (as.numeric(month) - 1)/12, y = observed_monthly_mean),
+             alpha = 0.6, color = 'lightgray', position = position_nudge(x = 0.015)) +
+  geom_ribbon(data = yearly_stats %>% filter(year > 2002 & year < 2020), mapping=aes(x=as.numeric(year), ymin=value_lo, ymax=value_hi), 
+              fill="grey60", color=NA, alpha=0.8) +
+  geom_line(data = yearly_stats %>% filter(year > 2002 & year < 2020),
+            aes(x = as.numeric(year), y = mean_temp),
+            color = "black", group = 1) +
+  # Heatwave
+  annotate(geom="rect", xmin=2013.5, xmax=2016.5, ymin=-Inf, ymax=Inf, fill="red", alpha=0.2) +
+  annotate(geom="text", label="MHW", x=2008, y=ymax , size=2.5) +
+  annotate("segment", x = 2010.5, y = ymax, xend = 2013.5, yend = ymax,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))+
+  labs(x = "Year", y = "Air temp (°C)",tag="A") +
+  scale_x_continuous(breaks = seq(2000, 2020, 5)) +
   scale_y_continuous() +
-  theme_bw() + my_theme
-g
+  theme_bw() + base_theme +
+  theme(axis.title.x=element_blank(),
+        axis.text.y = element_text(angle = 0, hjust = 0.5),
+        #plot.tag.position = c(0.05, 0.98)
+        )
 
-ggsave(g, filename=file.path(plotdir, "FigSX_air_temp_anamoly.png"), 
-       width=6, height=7, units="in", dpi=600)
+g1
 
 # SST
 #ymax <- envi %>% filter(indicator=="SST") %>% pull(value_hi) %>% max() * 1.05   ##Chris
-ymax <- max(envi_dat$sst_monthly_anom, na.rm=TRUE) * .90 #Josh
+ymax <- max(envi_dat$sst_monthly_obs, na.rm=TRUE) 
 g2 <- ggplot(data=envi %>% filter(indicator=="SST"), aes(x=year, y=value_avg)) + 
   #data series
-  geom_point(data=envi_dat, aes(x=date_order, y=sst_monthly_anom), size=1, alpha=0.1, color='lightgray')+
+  geom_point(data=envi_dat, aes(x=date_order, y=sst_monthly_obs), size=1, alpha=0.1, color='lightgray')+
   # Heatwave
   annotate(geom="rect", xmin=2013.5, xmax=2016.5, ymin=-Inf, ymax=Inf, fill="red", alpha=0.2) +
   annotate(geom="text", label="MHW", x=2008, y=ymax , size=2.5) +
@@ -146,26 +137,25 @@ g2 <- ggplot(data=envi %>% filter(indicator=="SST"), aes(x=year, y=value_avg)) +
               fill="grey60", color=NA, alpha=0.8) +
   geom_line() +
   #add data points
-  # Reference line
-  geom_hline(yintercept = 0, linetype="dashed", color="grey40") +
   # Labels
-  labs(x="", y="SST anamoly (°C)", tag="B") +
+  labs(x="", y="Sea surface temp (°C)", tag="B") +
   scale_x_continuous(breaks=seq(2000, 2020, 5)) +
   # scale_y_continuous(breaks=seq(-1.5, 1.5, 0.5), lim=c(-1.5, 1.5)) +
   # Theme
   theme_bw() + base_theme +
   theme(axis.title.x=element_blank(),
         axis.text.y = element_text(angle = 0, hjust = 0.5),
-        plot.tag.position = c(0.05, 0.98))
+        #plot.tag.position = c(0.05, 0.98)
+        )
 g2
 
 
 # bottomT
 #ymax <- envi %>% filter(indicator=="Bottom temp") %>% pull(value_hi) %>% max() * 1.05 #Chris
-ymax <- max(envi_dat$bottomT_monthly_anom, na.rm=TRUE) * .90 #Josh
+ymax <- max(envi_dat$bottomT_monthly_obs, na.rm=TRUE) 
 g3 <- ggplot(data=envi %>% filter(indicator=="Bottom temp"), aes(x=year, y=value_avg)) + 
   #data series
-  geom_point(data=envi_dat, aes(x=date_order, y=bottomT_monthly_anom), size=1, alpha=0.1, color='lightgray')+
+  geom_point(data=envi_dat, aes(x=date_order, y=bottomT_monthly_obs), size=1, alpha=0.1, color='lightgray')+
   # Heatwave
   annotate(geom="rect", xmin=2013.5, xmax=2016.5, ymin=-Inf, ymax=Inf, fill="red", alpha=0.2) +
   annotate(geom="text", label="MHW", x=2008, y=ymax , size=2.5) +
@@ -175,53 +165,25 @@ g3 <- ggplot(data=envi %>% filter(indicator=="Bottom temp"), aes(x=year, y=value
   geom_ribbon(mapping=aes(x=year, ymin=value_lo, ymax=value_hi), 
               fill="grey60", color=NA, alpha=0.8) +
   geom_line() +
-  # Reference line
-  geom_hline(yintercept = 0, linetype="dashed", color="grey40") +
   # Labels
-  labs(x="", y="SBT anamoly (°C)", tag="C") +
+  labs(x="", y="Sea bottom temp (°C)", tag="C") +
   scale_x_continuous(breaks=seq(2000, 2020, 5)) +
   # scale_y_continuous(breaks=seq(-1.5, 1.5, 0.5), lim=c(-1.5, 1.5)) +
   # Theme
   theme_bw() + base_theme +
   theme(axis.title.x=element_blank(),
         axis.text.y = element_text(angle = 0, hjust = 0.5),
-        plot.tag.position = c(0.05, 0.98))
+        #plot.tag.position = c(0.05, 0.98)
+        )
 g3
 
-# MOCI
-#ymax <- envi %>% filter(indicator=="MOCI") %>% pull(value_hi) %>% max() * 1.05 #Chris
-ymax <- max(envi_dat$quarterly_MOCI, na.rm=TRUE) * .90 #Josh
-g4 <- ggplot(data=envi %>% filter(indicator=="MOCI"), aes(x=year, y=value_avg)) + 
-  #data series
-  geom_point(data=envi_dat, aes(x=date_order, y=quarterly_MOCI), size=1, alpha=0.1, color='lightgray')+
-  # Heatwave
-  annotate(geom="rect", xmin=2013.5, xmax=2016.5, ymin=-Inf, ymax=Inf, fill="red", alpha=0.2) +
-  annotate(geom="text", label="MHW", x=2008, y=ymax , size=2.5) +
-  annotate("segment", x = 2010.5, y = ymax, xend = 2013.5, yend = ymax,
-           arrow = arrow(type = "closed", length = unit(0.02, "npc")))+
-  # Plot data
-  geom_ribbon(mapping=aes(x=year, ymin=value_lo, ymax=value_hi), 
-              fill="grey60", color=NA, alpha=0.8) +
-  geom_line() +
-  # Reference line
-  geom_hline(yintercept = 0, linetype="dashed", color="grey40") +
-  # Labels
-  labs(x="", y="MOCI", tag="D") +
-  scale_x_continuous(breaks=seq(2000, 2020, 5)) +
-  # scale_y_continuous(breaks=seq(-1.5, 1.5, 0.5), lim=c(-1.5, 1.5)) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(axis.title.x=element_blank(),
-        axis.text.y = element_text(angle = 0, hjust = 0.5),
-        plot.tag.position = c(0.05, 0.98))
-g4
 
 # BEUTI
 #ymax <- envi %>% filter(indicator=="BEUTI") %>% pull(value_hi) %>% max() * 1.05 #Chris
-ymax <- max(envi_dat$beuti_monthly_anom, na.rm=TRUE) * .90 #Josh
-g5 <- ggplot(data=envi %>% filter(indicator=="BEUTI"), aes(x=year, y=value_avg)) + 
+ymax <- max(envi_dat$beuti_monthly_obs, na.rm=TRUE) 
+g4 <- ggplot(data=envi %>% filter(indicator=="BEUTI"), aes(x=year, y=value_avg)) + 
   #data series
-  geom_point(data=envi_dat, aes(x=date_order, y=beuti_monthly_anom), size=1, alpha=0.1, color='lightgray')+
+  geom_point(data=envi_dat, aes(x=date_order, y=beuti_monthly_obs), size=1, alpha=0.1, color='lightgray')+
   # Heatwave
   annotate(geom="rect", xmin=2013.5, xmax=2016.5, ymin=-Inf, ymax=Inf, fill="red", alpha=0.2) +
   annotate(geom="text", label="MHW", x=2008, y=ymax , size=2.5) +
@@ -231,41 +193,26 @@ g5 <- ggplot(data=envi %>% filter(indicator=="BEUTI"), aes(x=year, y=value_avg))
   geom_ribbon(mapping=aes(x=year, ymin=value_lo, ymax=value_hi), 
               fill="grey60", color=NA, alpha=0.8) +
   geom_line() +
-  # Reference line
-  geom_hline(yintercept = 0, linetype="dashed", color="grey40") +
   # Labels
   #labs(x = "", y = expression("BEUTI anomaly\n"~(mmol~m^{-1}~s^{-1})), tag="E")+
-  labs(x = "", y = "BEUTI anomaly \n(mmol m\u207B\u00B9 s\u207B\u00B9)", tag="E")+
+  labs(x = "", y = "BEUTI (mmol m\u207B\u00B9 s\u207B\u00B9)", tag="D")+
   scale_x_continuous(breaks=seq(2000, 2020, 5)) +
   # scale_y_continuous(breaks=seq(-1.5, 1.5, 0.5), lim=c(-1.5, 1.5)) +
   # Theme
   theme_bw() + base_theme +
   theme(axis.title.x=element_blank(),
         axis.text.y = element_text(angle = 0),
-        plot.tag.position = c(0.05, 0.98))
-g5
+        #plot.tag.position = c(0.05, 0.98)
+        )
+g4
 
 
+g <- ggpubr::ggarrange(g1,g2,g3,g4, ncol=2, nrow=2)
 
-# CUTI
-ymax <- envi %>% filter(indicator=="CUTI") %>% pull(value_hi) %>% max() * 1.05
-g6 <- ggplot(data=envi %>% filter(indicator=="CUTI"), aes(x=year, y=value_avg)) + 
-  # Heatwave
-  annotate(geom="rect", xmin=2013.5, xmax=2016.5, ymin=-Inf, ymax=Inf, fill="red", alpha=0.5) +
-  annotate(geom="text", label="MHW", x=2015, y=ymax , size=2.1) +
-  # Plot data
-  geom_ribbon(mapping=aes(x=year, ymin=value_lo, ymax=value_hi), 
-              fill="grey60", color=NA, alpha=0.8) +
-  geom_line() +
-  # Reference line
-  geom_hline(yintercept = 0, linetype="dashed", color="grey40") +
-  # Labels
-  labs(x="", y="CUTI anamoly", tag="F") +
-  scale_x_continuous(breaks=seq(2000, 2020, 5)) +
-  # scale_y_continuous(breaks=seq(-1.5, 1.5, 0.5), lim=c(-1.5, 1.5)) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(axis.title.x=element_blank(),
-        axis.text.y = element_text(angle = 90, hjust = 0.5),
-        plot.tag.position = c(0.02, 1))
-g6
+g_final <- annotate_figure(g, bottom = textGrob("Year", vjust=-0.5, hjust=0))
+
+
+ggsave(g_final, filename=file.path(plotdir, "FigSX_absolute_temps.png"), 
+       width=6, height=6, units="in", dpi=600)
+
+
