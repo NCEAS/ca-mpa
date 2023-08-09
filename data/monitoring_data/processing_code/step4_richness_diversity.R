2973558467077cecbc0e167a24f5e140145b4865
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-08-09 12:33:07 -0700

    working

--- data/monitoring_data/processing_code/step4_richness_diversity.R
+++ data/monitoring_data/processing_code/step4_richness_diversity.R
@@ -16,17 +16,6 @@ kelp_raw <- read.csv(file.path(datadir, "kelpforest_fish_biomass.csv"))
 rocky_reef_raw <- read.csv(file.path(datadir, "ccfrp_fish_biomass.csv"))
 deep_reef_raw <- read.csv(file.path(datadir, "deep_reef_fish_biomass.csv"))
 
-################################################################################
-
-#THIS CHUNK MIGHT GET DELETED
-
-# Some site pairs do not appear in the data because they were either not sampled, 
-#or because no species were observed. True zeros should be included, so we need 
-#to use the site tables to identify where true zeros might exist. 
-
-#
-
-sites <- readRDS("/home/shares/ca-mpa/data/sync-data/monitoring/monitoring_sites_clean.Rds")
 
 ################################################################################
 # processing notes
@@ -46,9 +35,8 @@ sites <- readRDS("/home/shares/ca-mpa/data/sync-data/monitoring/monitoring_sites
 
 
 ################################################################################
-# calculate species diversity and richness
+# Surf zone
 
-#-------------------SURF ZONE--------------------------------------------------#
 #calculate effort for each MPA
 surf_effort <- surf_zone_raw %>%
   #keep track of hauls with no spp, but these get dropped below
@@ -131,15 +119,15 @@ surf_H_R <-surf_RR %>%
 
 
 
-#-------------------kelp forest------------------------------------------------#
+################################################################################
+# Kelp forest
 
-
-#calculate total biom for each rep unit
+#calculate effort
 kelp_effort <- kelp_raw %>%
   separate(sciname, into = c("genus", "species"), sep = " ")%>%
   #rename dummy var for any species to get dropped. Keep for now to calculate effort
-  mutate(species = ifelse(species == "spp" | is.na(species),"NOSP",species))%>%
-  #find number of hauls for each MPA
+  mutate(species = ifelse(species == "spp" | is.na(species),"NO_ORG",species))%>%
+  #find number of transects for each MPA
   distinct(year, bioregion, region4, affiliated_mpa,
            mpa_state_class, mpa_state_designation,
            mpa_defacto_class, mpa_defacto_designation, zone, level, transect)%>%
@@ -154,9 +142,6 @@ kelp_effort <- kelp_raw %>%
 #find total count of each species per MPA 
 kelp_diversity <- kelp_raw %>%
   separate(sciname, into = c("genus", "species"), sep = " ")%>%
-  #rename dummy var for any species to get dropped. Keep for now to calculate effort
-  mutate(species = ifelse(species == "spp" | is.na(species),"NOSP",species))%>%
-  filter(!(species == "NOSP"))%>%
   group_by(year, bioregion, region4, affiliated_mpa,
            mpa_state_class, mpa_state_designation,
            mpa_defacto_class, mpa_defacto_designation,
@@ -176,6 +161,14 @@ kelp_diversity <- kelp_raw %>%
   mutate(shannon_weighted = shannon_unweighted / n_transects,
          richness_weighted = richness_unweighted / n_transects)
 
+#check if there were any MPAs where no species were observed
+kelp_diversity_MPAs <- kelp_diversity %>% 
+  group_by(year, bioregion, region4, affiliated_mpa,
+           mpa_state_class, mpa_state_designation,
+           mpa_defacto_class, mpa_defacto_designation) %>% distinct()
+
+nrow(kelp_effort) - nrow(kelp_diversity_MPAs) #all MPAs had at least one fish. Yay!
+
 
 #Reshape
 kelp_RR <- kelp_diversity %>% 
@@ -194,19 +187,50 @@ kelp_RR <- kelp_diversity %>%
 kelp_H_R <-kelp_RR %>%
   rename("n_rep_ref" = n_transects_ref,
          "n_rep_smr" = n_transects_smr) %>%
-  mutate(habitat = "Kelp forest")
+  mutate(habitat = "Kelp forest") %>%
+  #some years have missing pairs, so these get dropped
+  filter(!(is.na(n_rep_ref) | is.na(n_rep_smr)))
 
+
+################################################################################
+# For CCFRP, need to calculate CPUE and then use CPUE for diversity
+
+#Everything below is copy over and needs to be complete 8/9/2023
 
 #------------------------CCFRP------------------------------------------------#
 
 #calculate total biom for each rep unit
-ccfrp_build1 <- rocky_reef_raw %>%
+ccfrp_effort <- rocky_reef_raw %>%
+  #keep track of cells with no spp, but these get dropped below
+  mutate(species = ifelse(species_code == "NOSP","NO_ORG",species))%>%
+  #drop species with NA
+  filter(!(is.na(species))) %>%
+  #find number of hauls for each MPA
+  distinct(year, bioregion, region4, affiliated_mpa,
+           mpa_state_class, mpa_state_designation,
+           mpa_defacto_class, mpa_defacto_designation, haul_number)%>%
+  
   #excluding target_status for CCFRP, since they are all targeted 
   group_by(year, bioregion, region4, affiliated_mpa,
            mpa_defacto_class, mpa_defacto_designation,
            grid_cell_id)%>%
   dplyr::summarize(total_biomass = sum(cell_bpue_kg)) 
 
+#calculate effort for each MPA
+surf_effort <- surf_zone_raw %>%
+  #keep track of hauls with no spp, but these get dropped below
+  mutate(species = ifelse(species_code == "NOSP","NO_ORG",species))%>%
+  #drop species with NA
+  filter(!(is.na(species))) %>%
+  #find number of hauls for each MPA
+  distinct(year, bioregion, region4, affiliated_mpa,
+           mpa_state_class, mpa_state_designation,
+           mpa_defacto_class, mpa_defacto_designation, haul_number)%>%
+  group_by(year, bioregion, region4, affiliated_mpa,
+           mpa_state_class, mpa_state_designation,
+           mpa_defacto_class, mpa_defacto_designation)%>%
+  dplyr::summarize(n_hauls = n())%>%
+  ungroup()
 
 #calculate MPA average and error
 
