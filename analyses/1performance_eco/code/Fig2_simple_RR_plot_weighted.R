401a1051a5748b688c6ffbc2e059547e3007104f
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-08-16 13:21:09 -0700

    working

--- analyses/1performance_eco/code/Fig2_simple_RR_plot_weighted.R
+++ analyses/1performance_eco/code/Fig2_simple_RR_plot_weighted.R
@@ -26,31 +26,59 @@ biomass_mod <- biomass_mod %>% mutate(target_status = ifelse(habitat == "Rocky r
                                         str_replace(" Smca$", " SMCA"))
 
 ################################################################################
-#create forest plot for pooled effect size by MPA, where effect size 
-#is calculated using age > 0 and weighted by no. of habitats included in that 
-#MPA
+##calcualte effect size for each year and MPA
+
+#this is the calculation of the variance without the use of escalc
+dat <- biomass_mod %>%
+          mutate(yi = logRR,
+                 vi = ((sd_smr^2) / (n_rep_smr*((biomass_smr + scalar_smr)^2))) +
+                   ((sd_ref^2) / (n_rep_ref*((biomass_ref + scalar_ref)^2))))
 
 #calcualte effect size for each year and MPA
-dat <- escalc(measure="ROM", m1i=biomass_smr + scalar_smr, m2i=biomass_ref + scalar_ref, sd1i=sd_smr, 
-              sd2i=sd_ref, n1i=n_rep_smr, n2i=n_rep_ref, data=biomass_mod)
+#dat <- escalc(measure="ROM", m1i=biomass_smr + scalar_smr, m2i=biomass_ref + scalar_ref, sd1i=sd_smr, 
+ #             sd2i=sd_ref, n1i=n_rep_smr, n2i=n_rep_ref, data=biomass_mod)
 
 
 forest_dat <- dat %>% filter(age_at_survey > 0) %>% 
   #drop missing variance
   filter(!(is.na(vi) | vi == 0))
 
+
+# Estimate the between-study variance (va) using DerSimonian-Laird estimator --- this is overall and not mpa specific
+#Q <- sum((forest_dat$yi - mean(forest_dat$yi))^2 / (forest_dat$vi + forest_dat$vi))
+#k <- nrow(forest_dat)
+#tau_squared <- max(0, (Q - (k - 1)) / sum(1 / (forest_dat$vi + forest_dat$vi)))
+
+# Calculate the Q statistic for each affiliated_mpa
+Q <- forest_dat %>%
+  group_by(affiliated_mpa) %>%
+  summarize(Q = sum((yi - mean(yi))^2 / vi))
+
+# Estimate the between-study variance (va) for each affiliated_mpa
+Q <- Q %>%
+  group_by(affiliated_mpa) %>%
+  mutate(va = max(0, (Q - (n() - 1)) / n()))
+
+# Merge the va values back into the original data
+forest_dat <- left_join(forest_dat, Q, by = "affiliated_mpa")
+
 ################################################################################
 #determine weighted means
 
 # Calculate the weighted effect size (weighted_yi)
+#forest_weighted <- forest_dat %>%
+ # mutate(weighted_yi = yi * (1/ vi + va)) #weight each effect size by the inverse of its variance and the between study variance
+
 forest_weighted <- forest_dat %>%
-  mutate(weighted_yi = yi * (1/vi)) #weight each effect size by the inverse of its variance
+  mutate(weighted_yi = yi * (1/ vi)) 
 
 # Calculate the sum of weighted variance for each target status and MPA
 pooled_mean <- forest_weighted %>%
   group_by(state_region, affiliated_mpa, target_status) %>%
   summarize(sum_weight = sum(1 / vi),
-            sum_weighted_yi = sum(weighted_yi))
+            sum_weighted_yi = sum(weighted_yi),
+            #sum_weighted_yi = sum(yi) # this is uneweighted
+            )
 
 # Calculate the pooled mean and standard error
 pooled_mean_data <- pooled_mean %>%
@@ -62,6 +90,7 @@ pooled_mean_data <- pooled_mean %>%
 pooled_mean_ci <- pooled_mean_data %>%
   mutate(lower_ci_yi = pooled_mean_yi - 1.96 * pooled_se_yi,
          upper_ci_yi = pooled_mean_yi + 1.96 * pooled_se_yi)
+
 
 # Calculate the number of unique habitats in each affiliated_mpa in the original data frame
 habitat_counts <- forest_dat %>%
@@ -154,6 +183,6 @@ g <- ggplot(mean_es_with_habitat_count,
 
 g
 
-ggsave(g, filename=file.path(fig_dir, "Fig2_mpa_effect_size5.png"), bg = "white",
-       width=7.5, height=9, units="in", dpi=600) 
+#ggsave(g, filename=file.path(fig_dir, "Fig2_mpa_effect_size5.png"), bg = "white",
+ #      width=7.5, height=9, units="in", dpi=600) 
 
