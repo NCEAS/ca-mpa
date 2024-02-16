f8891ade6466d5fa1bb2bf68a026753998b645f9
Joshua Smith <joshsmith@nceas.ucsb.edu>
2024-02-16 12:06:15 -0800

    organizing

--- analyses/1performance_eco/code/TableS4_sampling_effort.R
+++ analyses/1performance_eco/code/TableS4_sampling_effort.R
@@ -6,50 +6,20 @@ rm(list=ls())
 library(tidyverse)
 
 # Set directories 
-datadir <- "/home/shares/ca-mpa/data/sync-data/monitoring/processed_data/biomass_processed"
+dat_path <- here::here("analyses","1performance_eco","output")
 tabdir <- here::here("analyses","1performance_eco","tables")
 
 # Load data
-surf_biomass <- read.csv(file.path(datadir, "surf_zone_fish_biomass_updated.csv"))
-kelp_biomass <- read.csv(file.path(datadir, "kelpforest_fish_biomass_updated.csv")) 
-ccfrp_biomass <- read.csv(file.path(datadir, "ccfrp_fish_biomass_updated.csv"))
-deep_biomass <- read.csv(file.path(datadir, "deep_reef_fish_biomass_updated.csv"))
+biomass_mod <- readRDS(file.path(dat_path, "biomass_with_moderators_new2.Rds")) 
 
 ################################################################################
 #get all unique MPAs and types
 
-surf_mpas <- surf_biomass %>% select(affiliated_mpa, bioregion, year, mpa_state_class, 
-                                     mpa_defacto_class) %>% unique() %>%
-  mutate(Ecosystem = "Surf zone")
-
-
-kelp_mpas <- kelp_biomass %>% select(affiliated_mpa, bioregion, year, mpa_state_class, 
-                                     mpa_defacto_class) %>% unique() %>%
-  mutate(Ecosystem = "Kelp forest")
-
-ccfrp_mpas <- ccfrp_biomass %>% select(affiliated_mpa, bioregion,year,  mpa_state_class, 
-                                       mpa_defacto_class) %>% unique() %>%
-  mutate(Ecosystem = "Shallow reef")
-
-deep_mpas <- deep_biomass %>% select(affiliated_mpa, bioregion,year,  mpa_state_class, 
-                                     mpa_defacto_class) %>% unique() %>%
-  mutate(Ecosystem = "Deep reef")
-
-
-#join
-mpa_type <- rbind(surf_mpas, kelp_mpas, ccfrp_mpas, deep_mpas) %>%
-  #organize
-  select(Ecosystem, everything())%>%
-  mutate(affiliated_mpa = str_to_title(affiliated_mpa),
-         affiliated_mpa = str_replace(affiliated_mpa, "\\b(\\w+)$", function(x) toupper(x))) %>%
-  drop_na() %>%
-  mutate(mpa_state_class = toupper(mpa_state_class),
-         mpa_defacto_class = toupper(mpa_defacto_class)) %>%
-  select(Ecosystem, affiliated_mpa, year)
-
-
+mpa_effort <- biomass_mod %>% select(habitat, year, affiliated_mpa) %>% unique() %>%
+              rename(Ecosystem = habitat)
+             
 # Summarize the years into a single string for each affiliated_mpa and Ecosystem combination
-mpa_type_summary <- mpa_type %>%
+mpa_type_summary <- mpa_effort %>%
   group_by(affiliated_mpa, Ecosystem) %>%
   summarise(years = toString(unique(year)), .groups = 'drop')
 
