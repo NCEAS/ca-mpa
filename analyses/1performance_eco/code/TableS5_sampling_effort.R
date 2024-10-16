e286dc510abd7202584175d2453a92d63ac44025
Joshua Smith <joshsmith@nceas.ucsb.edu>
2024-10-16 17:15:44 +0000

    working

--- analyses/1performance_eco/code/TableS5_sampling_effort.R
+++ analyses/1performance_eco/code/TableS5_sampling_effort.R
@@ -8,6 +8,8 @@ library(tidyverse)
 # Set directories 
 dat_path <- here::here("analyses","1performance_eco","output")
 tabdir <- here::here("analyses","1performance_eco","tables")
+figdir <- here::here("analyses","1performance_eco","figures")
+
 
 # Load data
 biomass_mod <- readRDS(file.path(dat_path, "biomass_with_moderators_new2.Rds")) 
@@ -15,7 +17,7 @@ biomass_mod <- readRDS(file.path(dat_path, "biomass_with_moderators_new2.Rds"))
 ################################################################################
 #get all unique MPAs and types
 
-mpa_effort <- biomass_mod %>% select(habitat, year, affiliated_mpa) %>% unique() %>%
+mpa_effort <- biomass_mod %>% select(habitat, state_region, year, affiliated_mpa) %>% unique() %>%
               rename(Ecosystem = habitat)
              
 # Summarize the years into a single string for each affiliated_mpa and Ecosystem combination
@@ -30,6 +32,45 @@ mpa_type_wide <- mpa_type_summary %>%
 
 write.csv(mpa_type_wide, file.path(tabdir, "TableS4_survey_effort.csv"),row.names = FALSE)
 
+
+################################################################################
+#plot
+
+# Theme
+base_theme <- theme(axis.text=element_text(size=6, color = "black"),
+                    axis.title=element_blank(),
+                    strip.text=element_text(size=6.5, color = "black", face = "bold"),
+                    strip.background = element_blank(),
+                    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,color = "black",),
+                    # Gridlines
+                    panel.grid.major = element_blank(), 
+                    panel.grid.minor = element_blank(),
+                    panel.background = element_blank(), 
+                    axis.line = element_line(colour = "black"),
+                    # Legend
+                    legend.background = element_rect(fill=alpha('blue', 0)))
+
+
+g <- ggplot(mpa_effort %>%
+              mutate(state_region = recode(state_region,
+                                           "North Coast" = "North \nCoast",
+                                           "North Central Coast" = "North Central \nCoast",
+                                           "Central Coast" = "Central \nCoast",
+                                           "South Coast" = "South \nCoast"),
+                     Ecosystem = factor(Ecosystem, levels = c("Surf zone", "Kelp forest", "Shallow reef", "Deep reef"))),
+            aes(x = year, y = affiliated_mpa)) +
+  facet_grid(state_region ~ Ecosystem, scales = "free_y", space = "free_y") +
+  geom_tile(fill = "SteelBlue", width = 0.9, height = 0.9) +
+  # Labels
+  labs(x = "", y = "") +
+  scale_x_continuous(breaks = seq(2000, 2020, 5)) +
+  # Theme
+  theme_bw() + base_theme
+g
+
+# Export
+ggsave(g, filename=file.path(figdir, "FigS1_data_availability.png"), 
+       width=6.5, height=5.5, units="in", dpi=600)
 
 
 
