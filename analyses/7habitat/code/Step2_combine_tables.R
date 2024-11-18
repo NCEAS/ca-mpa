97a4ba236ce62f90dcf06d626e51546fe3e4f040
lopazanski <cori.lopazanski@gmail.com>
2024-11-18 14:37:23 +0000

    update rock complete table w habitat

--- analyses/7habitat/code/Step2_combine_tables.R
+++ analyses/7habitat/code/Step2_combine_tables.R
@@ -119,73 +119,38 @@ kelp <- kelp_raw %>%
 # 
 
 
-# CCFRP Rock ----------------------------------------------------------------------------------------------
+# Rock (CCFRP) ----------------------------------------------------------------------------------------------
 
-rock_raw <- readRDS(file.path(ltm.dir, "ccfrp_biomass_site_year.Rds")) %>% ungroup()
-rock_effort_raw <- readRDS(file.path(ltm.dir, "ccfrp_site_year_effort.Rds")) %>% ungroup()
+rock_raw <- readRDS(file.path(ltm.dir, "rock_biomass_complete.Rds")) 
 
 rock_sites <- rock_raw %>% 
-  distinct(year, site, affiliated_mpa, mpa_defacto_designation) %>% 
-  mutate(site_type = if_else(mpa_defacto_designation == "ref", "Reference", "MPA")) %>% 
-  left_join(mpas %>% dplyr::select(bioregion, affiliated_mpa, implementation_year)) %>% 
+  # Identify distinct site-year combinations
+  distinct(year, site, site_type, bioregion, affiliated_mpa, mpa_defacto_class, implementation_year) %>% 
   mutate(before = if_else(year <= implementation_year, 1, 0),
          after = if_else(year > implementation_year, 1, 0)) %>% 
-  group_by(site, bioregion, affiliated_mpa, implementation_year, site_type) %>% 
+  # Count number of years each site was visited before and after the MPA was implemented
+  group_by(site, bioregion, affiliated_mpa, mpa_defacto_class, implementation_year, site_type) %>% 
   summarize(n_before = sum(before),
-            n_after = sum(after),
-            n_total = n_before + n_after) %>% 
-  filter(!is.na(site_type)) %>% ungroup()
+            n_after = sum(after), # max visited is 16; most visited <5 times
+            n_total = n_before + n_after, .groups = 'drop') %>% 
+  # Decide to not filter grid cells because there are so many options for each MPA/Ref
+  left_join(habitat)
 
-habitat_rock <- habitat_raw %>% 
-  filter(habitat == "Rocky reef") %>% 
-  filter(site %in% rock_sites$site) %>% 
-  mutate(habitat_depth_buffer = paste(habitat_depth, buffer, sep = "_"))
-
-habitat_rock_wide <- habitat_rock %>% 
-  dplyr::select(mpa, affiliated_mpa, site, site_type, area_m2, habitat_depth_buffer) %>% 
-  pivot_wider(names_from = "habitat_depth_buffer", values_from = "area_m2") %>% 
-  dplyr::select(-affiliated_mpa, -site_type)
-
-rock_effort <- rock_effort_raw %>% 
-  filter(site %in% rock_sites$site) 
+rock_mpas <- rock_sites %>%
+  group_by(bioregion, affiliated_mpa, mpa_defacto_class, implementation_year, site_type) %>%
+  summarize(n_total = sum(n_total), .groups = 'drop') %>%
+  pivot_wider(names_from = site_type, values_from = n_total) %>% 
+  filter(MPA > 10 & Reference > 10) # at least 10 site-years; 30 MPAs total
 
 rock <- rock_raw %>% 
-  filter(site %in% rock_sites$site) %>% 
-  group_by(species_code, sciname, target_status, assemblage_new, year, bioregion, affiliated_mpa, site) %>% 
-  summarize(bpue_kg = sum(total_bpue_kg),
-            count = sum(total_count), .groups = 'drop') 
-
-rock_assemblage <- rock %>%
-  distinct(species_code, sciname, target_status, bioregion, assemblage_new)
-
-rock_subset <- rock %>% 
-  mutate(assemblage_new = case_when(sciname == "Paralabrax clathratus" ~ "Hard Bottom Biotic", 
-                                    sciname == "Sebastes miniatus" ~ "Hard Bottom", 
-                                    T~assemblage_new)) %>% 
-  filter(sciname %in% c("Semicossyphus pulcher", #sheephead
-                        "Caulolatilus princeps", # ocean whitefish
-                        "Sebastes atrovirens", # kelp rockfish
-                        "Ophiodon elongatus", # lingcod
-                        "Sebastes entomelas") | # widow rockfish
-           species_code %in% c("OYT", #olive yellowtail
-                               "PCLA" # kelp bass
-           )) 
-
-
-# Create Complete Kelp Data -------------------------------------------
-rock_complete <- rock_effort %>% 
-  expand_grid(species_code = unique(rock$species_code)) %>% 
-  left_join(rock %>% dplyr::select(-target_status, -assemblage_new, -sciname), by = c("year", "bioregion", "affiliated_mpa", "site", "species_code")) %>% 
-  left_join(rock %>% distinct(species_code, bioregion, sciname, target_status, assemblage_new), by = c("species_code", "bioregion")) %>% 
-  mutate_at(vars(kg_per_m2), ~ replace(., is.na(.), 0)) %>% 
-  mutate_at(vars(count_per_m2), ~ replace(., is.na(.), 0)) %>% 
-  left_join(kelp_sites %>% dplyr::select(site, region, affiliated_mpa, implementation_year, site_type)) %>% 
-  mutate(region = factor(region, levels = c("North Coast", "North Central Coast", "Central Coast", "South Coast")),
-         age_at_survey = year-implementation_year) %>% 
-  dplyr::select(year:mpa_defacto_designation, region, implementation_year, age_at_survey, site_type, species_code, sciname, target_status, assemblage_new, kg_per_m2, count_per_m2) %>% 
-  left_join(habitat_wide, by = "site") %>%
-  mutate(status = factor(if_else(age_at_survey < 0, "before", "after")))
-
+  # Drop observations for dropped MPAs
+  filter(affiliated_mpa %in% rock_mpas$affiliated_mpa) %>% 
+  # Drop before data
+  filter(age_at_survey >= 0) %>% 
+  # Join habitat and site visitation information
+  left_join(rock_sites) %>% 
+  # Log-transformed biomass
+  mutate(log_bpue_kg = log(weight_kg + 1))
 
 
 
