14e6bac80044ab2e13711579b50cecf678a3cdc0
lopazanski <cori.lopazanski@gmail.com>
2025-02-01 14:03:27 +0000

    fit surf and deep models

---
+++ analyses/7habitat/code/Step5c_fit_models_surf.R
@@ -0,0 +1,101 @@
+# Step5c Run Habitat Models
+# Cori Lopazanski
+# Jan 2025
+
+# This script will fit the surf zone models.
+
+library(tidyverse)
+library(lme4)
+library(MuMIn)
+library(dplyr)
+library(purrr)
+library(tidymodels)
+library(lmerTest)
+
+rm(list = ls())
+gc()
+
+source("analyses/7habitat/code/Step4_build_habitat_models.R")  # Load the function from the file
+
+
+# Read Data --------------------------------------------------------------------
+ltm.dir <- "/home/shares/ca-mpa/data/sync-data/monitoring/processed_data/update_2024"
+#ltm.dir <- "/Users/lopazanski/Desktop/ltm/update_2024"
+
+data_surf <- readRDS(file.path(ltm.dir, "combine_tables/surf_full.Rds")) %>% mutate(site_type = factor(site_type, levels = c("Reference", "MPA")))
+pred_surf <- readRDS(file.path("analyses/7habitat/intermediate_data/surf_predictors.Rds")) %>% filter(pred_group %in% c("all", "combined"))
+pred_surf_int <- readRDS(file.path("analyses/7habitat/intermediate_data/surf_predictors_interactions.Rds"))
+
+
+## Define Species Lists ------------------------------------------
+sp_surf <- data_surf %>%
+  filter(weight_kg > 0) %>%
+  group_by(species_code, sciname, target_status, bioregion) %>%
+  summarize(total_biomass = sum(weight_kg),
+            total_count = sum(count),
+            n_obs = n(), .groups = 'drop') %>%
+  filter(total_count > 30) %>% 
+  pivot_wider(names_from = bioregion, values_from = c(total_biomass, total_count, n_obs)) %>%
+  mutate(south = if_else(n_obs_South < 5 | is.na(n_obs_South), 0, 1),
+         central = if_else(n_obs_Central < 5 | is.na(n_obs_Central), 0, 1),
+         north = if_else(n_obs_North < 5 | is.na(n_obs_North), 0, 1))
+
+
+# Only model within the region they are prevalent in (defined as > 40 site-year observations)
+surf_all <- sp_surf %>% filter(south == 1 & central == 1 & north == 1) %>% pull(species_code)
+surf_s   <- sp_surf %>% filter(south == 1 & central == 0 & north == 0) %>% pull(species_code)
+surf_c   <- sp_surf %>% filter(south == 0 & central == 1 & north == 0) %>% pull(species_code)
+surf_n   <- sp_surf %>% filter(south == 0 & central == 0 & north == 1) %>% pull(species_code)
+surf_sc  <- sp_surf %>% filter(south == 1 & central == 1 & north == 0) %>% pull(species_code)
+surf_nc  <- sp_surf %>% filter(south == 0 & central == 1 & north == 1) %>% pull(species_code)
+
+
+# Define subset for modeling (reduced number of columns) 
+data_surf_subset <- data_surf %>%
+  dplyr::select(year:affiliated_mpa, size_km2, age_at_survey,
+                species_code:target_status, assemblage_new, weight_kg, count, kg_per_haul, count_per_haul, log_kg_per_haul,
+                all_of(pred_surf$predictor))
+
+# Fit models for each species --------------------------------------------------------------------
+library(furrr)
+library(parallel)
+
+# Combine all species into a single list
+species_list <- c(surf_all, surf_s, surf_c, surf_n, surf_sc, surf_nc)
+
+# Define a function to process a single species
+run_model <- function(species) {
+  # Determine the region
+  region <- 
+    if (species %in% surf_all) c("Central", "North", "South") else
+      if (species %in% surf_s) c("South") else
+        if (species %in% surf_c) c("Central") else
+          if (species %in% surf_n) c("North") else
+            if (species %in% surf_sc) c("South", "Central") else
+              if (species %in% surf_nc) c("North", "Central") else
+                stop("Species not found in any region group")
+  
+  # Run the habitat model
+  results_df <- refine_habitat(
+    species = species,
+    response = "log_c_biomass",
+    predictors_df = pred_surf_int,
+    random_effects = ifelse(length(region) > 1, 
+                            c("year", "bioregion", "affiliated_mpa"), 
+                            c("year", "affiliated_mpa")),
+    data = data_surf_subset,
+    regions = region,
+    path = "analyses/7habitat/output/surf"
+  )
+  
+  return(results_df)
+}
+
+# Plan for parallel execution
+num_cores <- min(length(species_list), detectCores()/3)  
+plan(multisession, workers = num_cores)
+
+# Run models in parallel
+future_walk(species_list, run_model)
+
+
--- analyses/7habitat/code/Step5d_fit_models_deep.R
+++ analyses/7habitat/code/Step5d_fit_models_deep.R
@@ -1,8 +1,8 @@
-# Step5 Run Habitat Models
+# Step5d Run Habitat Models
 # Cori Lopazanski
 # Jan 2025
 
-# This script will fit the kelp forest models.
+# This script will fit the deep reef models.
 
 library(tidyverse)
 library(lme4)
@@ -22,69 +22,70 @@ source("analyses/7habitat/code/Step4_build_habitat_models.R")  # Load the functi
 ltm.dir <- "/home/shares/ca-mpa/data/sync-data/monitoring/processed_data/update_2024"
 #ltm.dir <- "/Users/lopazanski/Desktop/ltm/update_2024"
 
-data_kelp <- readRDS(file.path(ltm.dir, "combine_tables/kelp_full.Rds")) %>% mutate(site_type = factor(site_type, levels = c("Reference", "MPA")))
-pred_kelp <- readRDS(file.path("analyses/7habitat/intermediate_data/kelp_predictors.Rds")) %>% filter(pred_group %in% c("all", "combined"))
-pred_kelp_int <- readRDS(file.path("analyses/7habitat/intermediate_data/kelp_predictors_interactions.Rds"))
+data_deep <- readRDS(file.path(ltm.dir, "combine_tables/ccfrp_full.Rds")) %>% mutate(site_type = factor(site_type, levels = c("Reference", "MPA")))
+pred_deep <- readRDS(file.path("analyses/7habitat/intermediate_data/deep_predictors.Rds")) %>% filter(pred_group %in% c("all", "combined"))
+pred_deep_int <- readRDS(file.path("analyses/7habitat/intermediate_data/deep_predictors_interactions.Rds"))
 
 
-# Define Kelp Species Lists --------------------------------------------------------------------------
-sp_kelp <- data_kelp %>%
-  filter(kg_per_m2 > 0) %>%
+## Define Species Lists ------------------------------------------
+sp_deep <- data_deep %>%
+  filter(weight_kg > 0) %>%
   group_by(species_code, sciname, target_status, bioregion) %>%
   summarize(total_biomass = sum(weight_kg),
             total_count = sum(count),
             n_obs = n(), .groups = 'drop') %>%
-  filter(n_obs > 40) %>%
+  filter(n_obs > 40) %>% 
   pivot_wider(names_from = bioregion, values_from = c(total_biomass, total_count, n_obs)) %>%
   mutate(south = if_else(n_obs_South < 40 | is.na(n_obs_South), 0, 1),
          central = if_else(n_obs_Central < 40 | is.na(n_obs_Central), 0, 1),
          north = if_else(n_obs_North < 40 | is.na(n_obs_North), 0, 1))
 
+
 # Only model within the region they are prevalent in (defined as > 40 site-year observations)
-kelp_all <- sp_kelp %>% filter(south == 1 & central == 1 & north == 1) %>% pull(species_code)
-kelp_s   <- sp_kelp %>% filter(south == 1 & central == 0 & north == 0) %>% pull(species_code)
-kelp_c   <- sp_kelp %>% filter(south == 0 & central == 1 & north == 0) %>% pull(species_code)
-kelp_n   <- sp_kelp %>% filter(south == 0 & central == 0 & north == 1) %>% pull(species_code)
-kelp_sc  <- sp_kelp %>% filter(south == 1 & central == 1 & north == 0) %>% pull(species_code)
-kelp_nc  <- sp_kelp %>% filter(south == 0 & central == 1 & north == 1) %>% pull(species_code)
+deep_all <- sp_deep %>% filter(south == 1 & central == 1 & north == 1) %>% pull(species_code)
+deep_s   <- sp_deep %>% filter(south == 1 & central == 0 & north == 0) %>% pull(species_code)
+deep_c   <- sp_deep %>% filter(south == 0 & central == 1 & north == 0) %>% pull(species_code)
+deep_n   <- sp_deep %>% filter(south == 0 & central == 0 & north == 1) %>% pull(species_code)
+deep_sc  <- sp_deep %>% filter(south == 1 & central == 1 & north == 0) %>% pull(species_code)
+deep_nc  <- sp_deep %>% filter(south == 0 & central == 1 & north == 1) %>% pull(species_code)
 
 
-# Define subset for modeling (reduced number of columns)
-data_kelp_subset <- data_kelp %>%
+# Define subset for modeling (reduced number of columns) 
+data_deep_subset <- data_deep %>%
   dplyr::select(year:affiliated_mpa, size_km2, age_at_survey,
-                species_code:target_status, assemblage_new, weight_kg:count_per_m2, log_kg_per_m2,
-                all_of(pred_kelp$predictor))
+                species_code:target_status, assemblage_new, weight_kg, count, log_bpue_kg,
+                all_of(pred_deep$predictor))
 
 # Fit models for each species --------------------------------------------------------------------
 library(furrr)
 library(parallel)
 
 # Combine all species into a single list
-species_list <- c(kelp_all, kelp_s, kelp_c, kelp_n, kelp_sc, kelp_nc)
+species_list <- c(deep_all, deep_s, deep_c, deep_n, deep_sc, deep_nc)
 
 # Define a function to process a single species
 run_model <- function(species) {
   # Determine the region
   region <- 
-    if (species %in% kelp_all) c("Central", "North", "South") else
-      if (species %in% kelp_s) c("South") else
-        if (species %in% kelp_c) c("Central") else
-          if (species %in% kelp_n) c("North") else
-            if (species %in% kelp_sc) c("South", "Central") else
-              if (species %in% kelp_nc) c("North", "Central") else
+    if (species %in% deep_all) c("Central", "North", "South") else
+      if (species %in% deep_s) c("South") else
+        if (species %in% deep_c) c("Central") else
+          if (species %in% deep_n) c("North") else
+            if (species %in% deep_sc) c("South", "Central") else
+              if (species %in% deep_nc) c("North", "Central") else
                 stop("Species not found in any region group")
   
   # Run the habitat model
   results_df <- refine_habitat(
     species = species,
     response = "log_c_biomass",
-    predictors_df = pred_kelp_int,
+    predictors_df = pred_deep_int,
     random_effects = ifelse(length(region) > 1, 
                             c("year", "bioregion", "affiliated_mpa"), 
                             c("year", "affiliated_mpa")),
-    data = data_kelp_subset,
+    data = data_deep_subset,
     regions = region,
-    path = "analyses/7habitat/output/kelp"
+    path = "analyses/7habitat/output/deep"
   )
   
   return(results_df)
