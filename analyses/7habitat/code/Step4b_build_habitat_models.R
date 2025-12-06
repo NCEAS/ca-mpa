00be3952974c6a3e7c6a67c009c24b36f213ab37
lopazanski <cori.lopazanski@gmail.com>
2025-12-06 16:28:00 +0000

    clean up

--- analyses/7habitat/code/Step4b_build_habitat_models.R
+++ analyses/7habitat/code/Step4b_build_habitat_models.R
@@ -31,11 +31,7 @@
 
 
 fit_habitat_models <- function(habitat, data_sp, response, focal_group, predictors_df, random_effects, re_string, path) {
-  
 
-  ## 2. Fit Models -----------------------------------------------------------------------
-  # models <- list()
-  
   models_df <- map_dfr(seq_len(nrow(predictors_df)), function(i) {
     predictors <- predictors_df$predictors[i]
     model_id <- predictors_df$model_id[i]
@@ -103,25 +99,6 @@ fit_habitat_models <- function(habitat, data_sp, response, focal_group, predicto
     mutate(messages = if_else(messages == "", NA, messages))
   
   print("  Models complete. Starting extraction.")
-  
-  # Extract the base model and full models for each scale
-  # core_model_names <- predictors_df %>%
-  #   filter(type == "base" | str_starts(type, "core")) %>%
-  #   pull(model_id)
-
-  # Extract the top models within deltaAICc of 10 
-  # top_model_names <- models_df %>%
-  #   filter(delta_AICc <= 6) %>%
-  #   pull(model_id)
-
-  # Extract the model objects for the core + top models
-  #models <- models[unique(c(top_model_names, core_model_names))]
-
-  # Define the top, core, and full models (but save entire df for debug)
-  # models_df <- models_df %>%
-  #   mutate(type = case_when(model_id %in% top_model_names ~ "top",
-  #                           predictors == "site_type * age_at_survey" ~ "base",
-  #                           model_id %in% core_model_names ~ "core"))
   
   # Find those that did not converge or had other issues
   problems <- models_df %>% 
